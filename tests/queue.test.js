41f745a0ebbcfb5fbddd4ef535b7dcf715232081
Ben Nelson <benn@ifit.com>
2017-04-07 23:09:29 -0600

    Add additional tests and hash-table helpers

--- tests/queue.test.js
+++ tests/queue.test.js
@@ -1,63 +1,72 @@
-// const Queue = require('../src/queue');
-// let queue;
+const Queue = require('../src/queue');
+let queue;
 
-// describe('Queue', () => {
+describe('Queue', () => {
   
-//   beforeEach(() => {
-//   	queue = new Queue();
-//   });
+  beforeEach(() => {
+  	queue = new Queue();
+  });
 
-//   it('should return a size of 0 for an empty queue', () => {
-//     expect(queue.size).toBe(0);
-//   });
+  it('should have the methods "enqueue", "dequeue", and the property "size"', () => {
+    const hasEnqueue = queue.hasOwnProperty('enqueue') || queue.__proto__.hasOwnProperty('enqueue') ? true : false;
+    const hasDequeue = queue.hasOwnProperty('dequeue') || queue.__proto__.hasOwnProperty('dequeue') ? true : false;
+    const hasSize = queue.hasOwnProperty('size') || queue.__proto__.hasOwnProperty('size') ? true : false;
+    expect(hasEnqueue).toBe(true);
+    expect(hasDequeue).toBe(true);
+    expect(hasSize).toBe(true);
+  });
 
-//   it('should return a size of 10 after queuing 10 items', () => {
-//     queue.enqueue(null);
-//     queue.enqueue(null);
-//     queue.enqueue(null);
-//     queue.enqueue(null);
-//     queue.enqueue(null);
-//     queue.enqueue(null);
-//     queue.enqueue(null);
-//     queue.enqueue(null);
-//     queue.enqueue(null);
-//     queue.enqueue(null);
-//     expect(queue.size).toBe(10);
-//   });
+  it('should return a size of 0 for an empty queue', () => {
+    expect(queue.size).toBe(0);
+  });
 
-//   it('should not error when attempting to dequeue an item from an empty queue', () => {
-//     expect(queue.dequeue()).toThrow(undefined);
-//   });
+  it('should return a size of 10 after queuing 10 items', () => {
+    queue.enqueue(null);
+    queue.enqueue(null);
+    queue.enqueue(null);
+    queue.enqueue(null);
+    queue.enqueue(null);
+    queue.enqueue(null);
+    queue.enqueue(null);
+    queue.enqueue(null);
+    queue.enqueue(null);
+    queue.enqueue(null);
+    expect(queue.size).toBe(10);
+  });
 
-//   it('should return a size of 0 after attempting to dequeue more items than were queued', () => {
-//     queue.dequeue();
-//     queue.dequeue();
-//     queue.dequeue();
-//     expect(queue.size).toBe(0);
-//   });
+  it('should not error when attempting to dequeue an item from an empty queue', () => {
+    expect(queue.dequeue()).toThrow(undefined);
+  });
 
-//   it('should dequeue and return the top item', () => {
-//     queue.enqueue(1);
-//     expect(queue.dequeue()).toBe(1);
-//   });
+  it('should return a size of 0 after attempting to dequeue more items than were queued', () => {
+    queue.dequeue();
+    queue.dequeue();
+    queue.dequeue();
+    expect(queue.size).toBe(0);
+  });
 
-//   it('should dequeue the first item queued if multiple items were queued', () => {
-//     queue.enqueue(true);
-//     queue.enqueue('hi');
-//     queue.enqueue(null);
-//     queue.enqueue(77);
-//     expect(queue.dequeue()).toBe(true);
-//   });
+  it('should dequeue and return the top item', () => {
+    queue.enqueue(1);
+    expect(queue.dequeue()).toBe(1);
+  });
 
-//   it('should respect the order with which elements are queued', () => {
-//     queue.enqueue(true);
-//     queue.enqueue('hi');
-//     queue.enqueue(null);
-//     queue.enqueue(77);
-//     expect(queue.dequeue()).toBe(true);
-//     expect(queue.dequeue()).toBe('hi');
-//     expect(queue.dequeue()).toBe(null);
-//     expect(queue.dequeue()).toBe(77);
-//   });
-// });
+  it('should dequeue the first item queued if multiple items were queued', () => {
+    queue.enqueue(true);
+    queue.enqueue('hi');
+    queue.enqueue(null);
+    queue.enqueue(77);
+    expect(queue.dequeue()).toBe(true);
+  });
+
+  it('should respect the order with which elements are queued', () => {
+    queue.enqueue(true);
+    queue.enqueue('hi');
+    queue.enqueue(null);
+    queue.enqueue(77);
+    expect(queue.dequeue()).toBe(true);
+    expect(queue.dequeue()).toBe('hi');
+    expect(queue.dequeue()).toBe(null);
+    expect(queue.dequeue()).toBe(77);
+  });
+});
 
--- tests/stack.test.js
+++ tests/stack.test.js
@@ -1,63 +1,72 @@
-// const Stack = require('../src/stack');
-// let stack;
+const Stack = require('../src/stack');
+let stack;
 
-// describe('Stack', () => {
+describe('Stack', () => {
 
-//   beforeEach(() => {
-//   	stack = new Stack();
-//   });
+  beforeEach(() => {
+  	stack = new Stack();
+  });
 
-//   it('should return a size of 0 for a new stack', () => {
-//     expect(stack.size).toBe(0);
-//   });
+  it('should have the methods "add", "remove", and the property "size"', () => {
+    const hasAdd = stack.hasOwnProperty('add') || stack.__proto__.hasOwnProperty('add') ? true : false;
+    const hasRemove = stack.hasOwnProperty('remove') || stack.__proto__.hasOwnProperty('remove') ? true : false;
+    const hasSize = stack.hasOwnProperty('size') || stack.__proto__.hasOwnProperty('size') ? true : false;
+    expect(hasAdd).toBe(true);
+    expect(hasRemove).toBe(true);
+    expect(hasSize).toBe(true);
+  });
 
-//   it('should return a size of 10 after adding 10 items to the stack', () => {
-//     stack.add(null);
-//     stack.add(null);
-//     stack.add(null);
-//     stack.add(null);
-//     stack.add(null);
-//     stack.add(null);
-//     stack.add(null);
-//     stack.add(null);
-//     stack.add(null);
-//     stack.add(null);
-//     expect(stack.size).toBe(10);
-//   });
+  it('should return a size of 0 for a new stack', () => {
+    expect(stack.size).toBe(0);
+  });
 
-//   it('should not error when attempting to remove an item from an empty stack', () => {
-//     expect(stack.remove()).toThrow(undefined);
-//   });
+  it('should return a size of 10 after adding 10 items to the stack', () => {
+    stack.add(null);
+    stack.add(null);
+    stack.add(null);
+    stack.add(null);
+    stack.add(null);
+    stack.add(null);
+    stack.add(null);
+    stack.add(null);
+    stack.add(null);
+    stack.add(null);
+    expect(stack.size).toBe(10);
+  });
 
-//   it('should return a size of 0 after attempting to remove more items than were added', () => {
-//     stack.remove();
-//     stack.remove();
-//     stack.remove();
-//     expect(stack.size).toBe(0);
-//   });
+  it('should not error when attempting to remove an item from an empty stack', () => {
+    expect(stack.remove()).toThrow(undefined);
+  });
 
-//   it('should remove and return the top item', () => {
-//     stack.add(1);
-//     expect(stack.remove()).toBe(1);
-//   });
+  it('should return a size of 0 after attempting to remove more items than were added', () => {
+    stack.remove();
+    stack.remove();
+    stack.remove();
+    expect(stack.size).toBe(0);
+  });
 
-//   it('should remove the most recent item added if multiple items added', () => {
-//     stack.add(true);
-//     stack.add('hi');
-//     stack.add(null);
-//     stack.add(77);
-//     expect(stack.remove()).toBe(77);
-//   });
+  it('should remove and return the top item', () => {
+    stack.add(1);
+    expect(stack.remove()).toBe(1);
+  });
 
-//   it('should respect the order with which elements are added', () => {
-//     stack.add(true);
-//     stack.add('hi');
-//     stack.add(null);
-//     stack.add(77);
-//     expect(stack.remove()).toBe(77);
-//     expect(stack.remove()).toBe(null);
-//     expect(stack.remove()).toBe('hi');
-//     expect(stack.remove()).toBe(true);
-//   });
-// });
+  it('should remove the most recent item added if multiple items added', () => {
+    stack.add(true);
+    stack.add('hi');
+    stack.add(null);
+    stack.add(77);
+    expect(stack.remove()).toBe(77);
+  });
+
+  it('should respect the order with which elements are added', () => {
+    stack.add(true);
+    stack.add('hi');
+    stack.add(null);
+    stack.add(77);
+    expect(stack.remove()).toBe(77);
+    expect(stack.remove()).toBe(null);
+    expect(stack.remove()).toBe('hi');
+    expect(stack.remove()).toBe(true);
+  });
+});
 
