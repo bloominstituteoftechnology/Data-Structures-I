4388791f9a0361c25c7955a32283b54348ceaaaa
sunjieming <ben@lambdaschool.com>
2017-04-15 18:57:17 -0600

    Add solutions

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,20 @@
+/* eslint-disable */
 class Stack {
+  constructor() {
+    this.storage = [];
+  }
 
+  get size() {
+    return this.storage.length;
+  }
+
+  add(item) {
+    this.storage.push(item);
+  }
+
+  remove() {
+    return this.storage.pop();
+  }
 }
 
 module.exports = Stack;
