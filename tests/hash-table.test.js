fedbf09998423bb96d2bcbfe5450e6741de9282d
sunjieming <ben@lambdaschool.com>
2017-04-15 18:56:19 -0600

    Move test to EC section

--- tests/hash-table.test.js
+++ tests/hash-table.test.js
@@ -22,22 +22,10 @@ describe('HashTable', () => {
     expect(hashTable.retrieve('hello')).toBe('there');
   });
 
-  it('should save and return values by key', () => {
-    hashTable.insert('hello', 'there');
-    expect(hashTable.retrieve('hello')).toBe('there');
-  });
-
   it('should properly remove items', () => {
     hashTable.insert('Ben', 'Nelson');
     hashTable.remove('Ben');
-    expect(hashTable.retrieve('Ben')).toBe(null);
-  });
-
-  it('should properly handle collisions', () => {
-    hashTable.insert('B', 'First Value');
-    hashTable.insert('HI!', 'Second Value');
-    expect(hashTable.retrieve('B')).toBe('First Value');
-    expect(hashTable.retrieve('HI!')).toBe('Second Value');
+    expect(hashTable.retrieve('Ben')).toBe(undefined);
   });
 
   it('should handle numbers as keys', () => {
@@ -47,6 +35,21 @@ describe('HashTable', () => {
     expect(hashTable.retrieve(1)).toBe('Second Value');
   });
 
+  it('should overwrite the value when using the same key', () => {
+    hashTable.insert(0, 'First Value');
+    hashTable.insert(0, 'Second Value');
+    expect(hashTable.retrieve(0)).toBe('Second Value');
+  });
+
+  /* Extra Credit */
+
+  // it('should properly handle collisions', () => {
+  //   hashTable.insert('B', 'First Value');
+  //   hashTable.insert('HI!', 'Second Value');
+  //   expect(hashTable.retrieve('B')).toBe('First Value');
+  //   expect(hashTable.retrieve('HI!')).toBe('Second Value');
+  // });
+  //
   // it('should resize the hash table when > 75% full', () => {
   //   hashTable.insert('a', true);
   //   hashTable.insert('b', true);
