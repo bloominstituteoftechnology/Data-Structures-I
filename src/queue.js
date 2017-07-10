10c74c6260819dfd07ce8ee3f03821a611699a4e
Latoyya Smith <latoyyas@gmail.com>
2017-07-10 17:57:59 -0500

    finished queue.js

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,17 @@
 class Queue {
-
+  constructor() {
+    this.queue = [];
+  }
+  enqueue(value) {
+    this.queue.push(value);
+  }
+  dequeue() {
+    return this.queue.shift();
+  }
+  get size() {
+    const size = this.queue.length;
+    return size;
+  }
 }
 
 module.exports = Queue;
