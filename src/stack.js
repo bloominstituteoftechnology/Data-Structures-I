9e762872dd6624bb955fbdb69e53cae89a9c128e
Ely Alamillo <el0013@gmail.com>
2017-07-10 17:30:41 -0500

    finished stack

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,19 @@
 class Stack {
+  constructor() {
+    this.stack = [];
+  }
 
+  add(value) {
+    this.stack.push(value);
+  }
+  remove() {
+    const valueRemoved = this.stack.pop();
+    return valueRemoved;
+  }
+  get size() {
+    const size = this.stack.length;
+    return size;
+  }
 }
 
 module.exports = Stack;
