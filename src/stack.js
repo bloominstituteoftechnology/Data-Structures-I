9bd76691088dfd8ac72e383b972d78c5661ba19f
sfmajors373 <sfmajors373@gmail.com>
2017-07-11 00:23:29 +0000

    stack: all but test 4 passing, no linting

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,21 @@
 class Stack {
-
+  constructor(options) {
+    this.stack = [];
+    this.size = 0;
+  }
+  add(node) {
+    this.stack.push(node);
+    this.size = this.stack.length;
+  }
+  remove() {
+    if (this.stack.length >= 1) {
+      const popped = this.stack.pop();
+      this.size = this.stack.length;
+      return popped;
+    }
+    return 0;
+  }
+  size() { return this.size };
 }
-
+const stack = new Stack();
 module.exports = Stack;
