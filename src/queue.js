d058f76390099574e37bdbb67ca09ae09a2a7887
sfmajors373 <sfmajors373@gmail.com>
2017-07-11 18:34:31 +0000

    queue: tests pass

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,24 @@
 class Queue {
-
+  constructor(options) {
+    this.queue = [];
+    this.size = 0;
+  }
+  enqueue(items) {
+    this.queue.push(items);
+    this.size = this.queue.length;
+  }
+  dequeue() {
+    if (this.queue.length) {
+      const shift = this.queue.shift();
+      this.size = this.queue.length;
+      return shift;
+    }
+    return Queue;
+  }
+  size() {
+    return this.size;
+  }
 }
 
+const queue = new Queue();
 module.exports = Queue;
