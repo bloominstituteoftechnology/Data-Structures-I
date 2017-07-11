9c988c6bda422dda0e454ff51cab9697c4737712
mixelpixel <mixelpix@mac.com>
2017-07-10 22:44:32 -0400

    all tests pass

--- src/stack.js
+++ src/stack.js
@@ -1,40 +1,23 @@
 class Stack {
   constructor() {
     this.storage = [];
-    // this.size = this.stack.length;
   }
-  size() { return this.storage.length; }
-  add() { return this.storage.push(); }
+  get size() { return this.storage.length; }
+  add(x) { return this.storage.push(x); }
   remove() { return this.storage.pop(); }
 }
 
-class User {
-  constructor(poop) {
-    // set a username and password property on the user object that is created
-    this.username = poop['username'];
-    this.password = poop.password;
-  }
-  // create a method on the User class called `checkPassword`
-  // this method should take in a string and compare it to the object's password property
-  // return `true` if they match, otherwise return `false`
-  checkPwd(compareString) {
-    return compareString === this.password;
-  }
-}
-
-const me = new User({ username: 'LambdaSchool', password: 'correcthorsebatterystaple' });
-// true case
-const result = me.checkPwd('correcthorsebatterystaple'); // should return `true`
-console.log(result);// ---> true
-// false case
-const falseResult = me.checkPwd('staplehorsecorrectbattery'); // should return `false`
-console.log(falseResult);// ---> false
-
-const someoneElse = new User({username: 'Bob', password: 'blob'})
-
-
-
-
-
+// const x = new Stack;
+// console.log(x);
+// console.log(x.size);
+// x.add('poop');
+// console.log(x.size);
+// console.log(x.storage);
+// console.log(x.remove());
+// console.log(x.size);
+// console.log(x.storage);
+// console.log(x.remove());
+// console.log(x.size);
+// console.log(x.storage);
 
 module.exports = Stack;
