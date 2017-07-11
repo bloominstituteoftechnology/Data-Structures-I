58854a9d0d3142a88b48f2d51f7f5a8e501727a6
sfmajors373 <sfmajors373@gmail.com>
2017-07-11 22:42:38 +0000

    linked list: all test pass and linted

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,6 +1,6 @@
 class Node {
-  constructor (element) {
-    this.value= element;
+  constructor(element) {
+    this.value = element;
     this.next = null;
   }
 }
@@ -10,40 +10,27 @@ class LinkedList {
     this.tail = null;
     // Do not modify anything inside of the constructor
   }
-  addToTail(element){
-    //take previous tail, add pointer to new tail
-	//is there a head? No----> make this the head, Yes -----> extend the tail?
+  addToTail(element) {
     const thingToAdd = new Node(element);
     let checkNode = this.head;
-    if(!checkNode) {
+    if (!checkNode) {
       this.head = thingToAdd;
       this.tail = this.head;
       return thingToAdd;
-    } 
-    while(checkNode.next) {
+    }
+    while (checkNode.next) {
       checkNode = checkNode.next;
-    }  
+    }
     checkNode.next = thingToAdd;
     this.tail = checkNode.next;
-    console.log(this.tail);
     return this.tail;
   }
-//	else if (this.tail === null) {
-//      this.head.next = thingToAdd;
-//      this.tail = thingToAdd;
-//    } else {
-//      this.tail.next = thingToAdd;
-//      this.tail = thingToAdd;
-//    }
-//  }
-  removeHead(){
-    //take head, remove it, make its pointer the next head, return the removed head
-    const nodeBeingRemoved = this.head;
+  removeHead() {
+    const nodeBeingRemoved = this.head.value;
     this.head = this.head.next;
     return nodeBeingRemoved;
-  } 
-  contains(item){
-    //takes item as argument, traverses list to find item, return true if it exists, false otherwise
+  }
+  contains(item) {
     let currentNode = this.head;
     while (currentNode.next !== null) {
       if (currentNode.value === item) {
