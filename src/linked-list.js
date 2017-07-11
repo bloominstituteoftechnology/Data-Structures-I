4e5bf2205e096c7933693fd9270341edbcbd47ff
Ely Alamillo <el0013@gmail.com>
2017-07-10 21:37:45 -0500

    finished linked list

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,5 +1,4 @@
-/*eslint no-console: ["error", { allow: ["warn", "error"] }] */
-
+/* eslint no-console: ["error", { allow: ["warn", "error"] }] */
 
 class LinkedList {
   constructor() {
@@ -8,38 +7,50 @@ class LinkedList {
     // Do not modify anything inside of the constructor
   }
   addToTail(value) {
-    this.tail = value;
-
     const node = {
-      value: value,
+      value,
       next: null
-    }
-
+    };
     if (!this.head) {
       this.head = node;
-    }
-    else {
+    } else {
       let current = this.head;
-      while(current.next) {
+      while (current.next) {
         current = current.next;
       }
       current.next = node;
     }
+    this.tail = node;
   }
-
   removeHead() {
-    console.log('this.next: ' + this.next);
-    console.log('this.head: ' + this.head)
+    const current = this.head;
+    if (current.value) {
+      this.head = this.head.next;
+    }
+    return current.value;
   }
-
-  contains() {
-
+  contains(value) {
+    let current = this.head;
+    while (current.next) {
+      if (current.value === value) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
   }
 }
-const list = new LinkedList;
-list.addToTail(1);
-list.addToTail(2);
-console.log(list);
-list.removeHead();
-console.log(list);
+const list = new LinkedList();
+// list.addToTail(1);
+// list.addToTail(2);
+// console.log(list);
+// //list.removeHead();
+// console.log(list);
+// console.log(list.contains(1))
+// console.log(list.contains(5))
+// list.addToTail(1);
+// list.addToTail(2);
+//
+// // console.log(list.tail.value);
+// console.log(list);
 module.exports = LinkedList;
