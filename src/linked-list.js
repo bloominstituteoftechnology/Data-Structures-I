8e9638648973ac6312245c402992d4e85365017b
LOLH <lincolnlaw@mac.com>
2017-07-11 06:36:22 -0700

    Initial commit stack, queue, linked-list

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,8 +1,48 @@
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class LinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
     // Do not modify anything inside of the constructor
+  }
+
+  addToTail(value) {
+    const newNode = new Node(value);
+    if (!this.tail) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  removeHead() {
+    if (!this.head) return null;
+    const headValue = this.head.value;
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+    }
+    return headValue;
+  }
+
+  contains(value) {
+    const check = (node) => {
+      if (!node) return false;
+      if (node.value === value) return true;
+      return check(node.next);
+    };
+    return check(this.head);
   }
 }
 
--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,19 @@
 class Queue {
+  constructor() {
+    this.structure = [];
+  }
 
+  get size() {
+    return this.structure.length;
+  }
+
+  enqueue(value) {
+    this.structure.unshift(value);
+  }
+
+  dequeue() {
+    return this.structure.pop();
+  }
 }
 
 module.exports = Queue;
