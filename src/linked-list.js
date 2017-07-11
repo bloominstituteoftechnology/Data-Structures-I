32775369bcb7e8301355f12bb1a8cf0577bc1e78
Huck <evanallen51@gmail.com>
2017-07-11 14:48:27 -0500

    v2

--- src/linked-list.js
+++ src/linked-list.js
@@ -4,6 +4,35 @@ class LinkedList {
     this.tail = null;
     // Do not modify anything inside of the constructor
   }
+  addToTail(newValue) {
+    if (this.head === null) {
+      this.head = {
+        value: newValue,
+        next: null
+      };
+      this.tail = this.head;
+    } else {
+      this.tail.next = LinkedList.constructor();
+      this.tail = this.tail.next;
+    }
+    this.tail.value = newValue;
+  }
+  removeHead() {
+    const rem = this.head;
+    this.head = this.head.next;
+    return rem.value;
+  }
+  contains(item) {
+    let current = this.head;
+    // return this.current.next.value;
+    while (this.current.value !== item) {
+      current = this.current.next;
+    }
+    if (this.current.value !== item) {
+      return false;
+    }
+    return true;
+  }
 }
 
 module.exports = LinkedList;
--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,19 @@
 class Queue {
-
+  constructor() {
+    this.arr = [];
+    this.size = 0;
+  }
+  enqueue(num) {
+    ++this.size;
+    this.arr.unshift(num);
+  }
+  dequeue() {
+    if (this.size > 0) --this.size;
+    return this.arr.pop();
+  }
+  size() {
+    return this.size;
+  }
 }
 
 module.exports = Queue;
