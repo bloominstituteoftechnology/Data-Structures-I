74aa9eafded4a0bd6de845e3b8da026fcbbf2b29
Ely Alamillo <el0013@gmail.com>
2017-07-10 19:22:12 -0500

    updated addToTail

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,9 +1,45 @@
+/*eslint no-console: ["error", { allow: ["warn", "error"] }] */
+
+
 class LinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
     // Do not modify anything inside of the constructor
   }
-}
+  addToTail(value) {
+    this.tail = value;
 
+    const node = {
+      value: value,
+      next: null
+    }
+
+    if (!this.head) {
+      this.head = node;
+    }
+    else {
+      let current = this.head;
+      while(current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+  }
+
+  removeHead() {
+    console.log('this.next: ' + this.next);
+    console.log('this.head: ' + this.head)
+  }
+
+  contains() {
+
+  }
+}
+const list = new LinkedList;
+list.addToTail(1);
+list.addToTail(2);
+console.log(list);
+list.removeHead();
+console.log(list);
 module.exports = LinkedList;
