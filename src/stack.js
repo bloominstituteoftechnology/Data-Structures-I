d1278c5b67b07814ca9c3100e34af266bb69ae60
David Friedman <mrdavidfriedman@gmail.com>
2017-07-11 17:01:53 +0000

    Changes

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,27 @@
-class Stack {
+'use strict';
 
+class Stack {
+    constructor() {
+        // empty stack
+        this.stack=[];
+    }
+
+    add(item) {
+        // add item to the stack
+        this.stack.push(item);
+    }
+
+    remove() {
+        // if there's an item to remove from the stack, remove it
+        if(!this.stack.length <= 0) {
+            return this.stack.pop();
+        }
+    }
+
+    size() {
+        // return the size of the stack
+        return this.stack.length;
+    }
 }
 
 module.exports = Stack;
