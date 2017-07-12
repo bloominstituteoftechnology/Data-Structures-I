601b17ca859f2c14dbea4446914de7c92157ff60
mixelpixel <mixelpix@mac.com>
2017-07-12 16:42:08 -0400

    We actually understand this code

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,3 +1,21 @@
+// // First value passed in to linked list:
+// [ {value: 'The Godfather', next: null, STATE {head:Godf, tail: Godf}},]
+//
+// // Second value added linked list:
+// [ {value: 'The Godfather', next: Matrix, STATE {head:Godf,/*tail:nil*/}},
+//   {value: 'The Matrix',    next: null,   STATE/*head: nil*/{tail: Matrix}} ]
+//
+// // Third value added to linked list:
+// [ {value: 'The Godfather', next: Matrix, STATE {head:Godf,/*tail:nil*/}},
+//   {value: 'The Matrix',    next: Ex Mac,/*STATE head: nil,tail:nil*/},
+//   {value: 'Ex Machina',    next: null,   STATE/* head */{tail: Ex}} ]
+//
+// // Fourth value added to linked list:
+// [ {value: 'The Godfather', next: Matrix, STATE {head:Godf,/*tail:nil*/}},
+//   {value: 'The Matrix',    next: Ex Mac,/*STATE head:nil,tail:nil*/},
+//   {value: 'Ex Machina',    next: OffSpac,/*STATE head:nil,tail:nil*/},
+//   {value: 'Office Space',  next: null,    STATE/* head:nil */{tail: OffSpac}} ]
+
 class LinkedList {
   constructor() {
     this.head = null;
@@ -6,24 +24,10 @@ class LinkedList {
     // Do not modify anything inside of the constructor
   }
 
-  // First value passed in to linked list:
-  [ {value: 'The Godfather', next: null, STATE {head:Godf, tail: Godf}},]
-
-  // Second value added linked list:
-  [ {value: 'The Godfather', next: Matrix, STATE {head:Godf,/*tail:nil*/}},
-    {value: 'The Matrix',    next: null,   STATE/*head: nil*/{tail: Matrix}} ]
-
-  // Third value added to linked list:
-  [ {value: 'The Godfather', next: Matrix, STATE {head:Godf,/*tail:nil*/}},
-    {value: 'The Matrix',    next: Ex Mac,/*STATE head: nil,tail:nil*/},
-    {value: 'Ex Machina',    next: null,   STATE/* head */{tail: Ex}} ]
-
-  // Fourth value added to linked list:
-  [ {value: 'The Godfather', next: Matrix, STATE {head:Godf,/*tail:nil*/}},
-    {value: 'The Matrix',    next: Ex Mac,/*STATE head:nil,tail:nil*/},
-    {value: 'Ex Machina',    next: OffSpac,/*STATE head:nil,tail:nil*/},
-    {value: 'Office Space',  next: null,    STATE/* head:nil */{tail: OffSpac}} ]
-
+  // Conditions to check to Add to tail
+  // 1) nothing in the list?
+  // 2) 1 other in the list?
+  // 3) more than 1?
   addToTail(fromOutside) {
     const node = {
       value: fromOutside, // <--- e.g. 'The Godfather'
@@ -34,29 +38,20 @@ class LinkedList {
     } else {
       this.head.next = node.value;
     }
+    
     this.tail = node;
   }
-
-  // Conditions to check to Add to tail
-  // 1) nothing in the list?
-  // 2) 1 other in the list?
-  // 3) more than 1?
-
-  removeHead() { /* removes and returns the head node. */
-    return this;
-  }
-  contains(x) { /* should searth through the linked list and return true if a matching value is found. */
-    return this;
-  }
+  removeHead() { return this; }/* removes and returns the head node. */
+  contains(x) { return this; }/* should searth through the linked list and return true if a matching value is found. */
 }
+const test = new LinkedList;
+test.addToTail('The Godfather');
+// console.log(test);
+test.addToTail('The Matrix');
+test.addToTail('Ex Machina');
+console.log(test);
 
-// const test = new LinkedList;
-// test.addToTail('argument');
-// console.log(test);
-// test.addToTail('heated conversation');
-// console.log(test);
 /*
-
 * 1) Should have the methods: `addToTail`, `removeHead`, and `contains`.
 * 2) `addToTail` replaces the tail with a new value that is passed in.
 * 3) `removeHead` removes and returns the head node.
@@ -65,15 +60,5 @@ class LinkedList {
 * the last node.  These are the only two properties that you need to keep track of an infinite number
 * of nodes.  Build your nodes with objects.
 */
-
-// class ExampleLinkedList {
-//   constructor() {
-//     this.head = {
-//       value: 'something',
-//       next: {}
-//     };
-//     this.tail = null;
-//   }
-// }
 
 module.exports = LinkedList;
