aba18368cfabbae68d442675be3640c305a717be
wlharvey4 <wlharvey4@mac.com>
2017-07-12 12:04:21 -0700

    hash-table.js

--- src/hash-table.js
+++ src/hash-table.js
@@ -7,6 +7,23 @@ class HashTable {
     this.storage = new LimitedArray(this.limit);
     // Do not modify anything inside of the constructor
   }
+
+  index(key) {
+    if (typeof key === 'number') key = key.toString();
+    return getIndexBelowMax(key, this.limit);
+  }
+
+  insert(key, value) {
+    this.storage.set(this.index(key), value);
+  }
+
+  remove(key) {
+    this.storage.set(this.index(key), undefined);
+  }
+
+  retrieve(key) {
+    return this.storage.get(this.index(key));
+  }
 }
 
 module.exports = HashTable;
