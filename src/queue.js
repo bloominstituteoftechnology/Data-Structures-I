044242013527fa2befdd566915aeb33304bac434
mixelpixel <mixelpix@mac.com>
2017-07-12 20:23:54 -0400

    all tests pass

--- src/queue.js
+++ src/queue.js
@@ -6,7 +6,6 @@ class Queue {
   enqueue(listItem) { return this.storage.push(listItem); }
   dequeue() { return this.storage.shift(); }
 }
-
 // * Should have the methods: `enqueue`, `dequeue`, and a getter for the property `size`
 // * `enqueue` should add an item to the back of the queue.
 // * `dequeue` should remove an item from the front of the queue.
