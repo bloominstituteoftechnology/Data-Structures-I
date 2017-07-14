faae0b5e33d5c3d01cde02dc190a394bc2cc6b03
Colombianito66 <colombianito66@gmail.com>
2017-07-14 19:57:27 -0400

    Stacks - all tests passing

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,18 @@
 class Stack {
+  constructor() {
+    this.array = [];
+  }
+  add(element) {
+    return this.array.push(element);
+  }
 
+  remove() {
+    return this.array.pop();
+  }
+
+  get size() {
+    return this.array.length;
+  }
 }
 
 module.exports = Stack;
