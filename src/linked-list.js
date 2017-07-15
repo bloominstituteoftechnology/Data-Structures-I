168e84be0cfd96f0ceb531e4983c6c5c01868849
Antonio Melendez <antoniomelendez@Antonios-MacBook-Pro.local>
2017-07-15 17:45:25 -0600

    Linked List - All passing

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,55 +1,48 @@
 class LinkedList {
-  /*
-  * Should have the methods: `addToTail`, `removeHead`, and `contains`.
-  * `addToTail` replaces the tail with a new value that is passed in.
-  * `removeHead` removes and returns the head node.
-  * `contains` should search through the linked list and return true if a matching value is found.
-  * The `head` property is a reference to the first node and the `tail` property is a reference to the last node.  These are the only two properties that you need to keep track of an infinite number of nodes.  Build your nodes with objects.
-  */
   constructor() {
     this.head = null;
     this.tail = null;
-    // Do not modify anything inside of the constructor
   }
   addToTail(itemToInsert) {
-    // v1 - nope
-    // const newNode = null;
-    // const current = this.tail;
-    // newNode.next = current.next;
-    // current.next = newNode;
     const node = {
       value: itemToInsert,
       next: null
     };
-
+    // insert 1 in the head
     if (this.head === null) {
-      // console.log('inserting head:', itemToInsert);
       this.head = node;
-    } else {
-      // console.log('inserting head next:', itemToInsert);
-      this.head.next = node.value;
+      this.tail = this.head;
+      return;
     }
-    // console.log('inserting tail:', itemToInsert);
+    // insert head.next
+    if (this.head.next === null) {
+      this.tail = node;
+      this.head.next = node;
+      return;
+    }
+    const tail = this.tail;
+    tail.next = node;
     this.tail = node;
   }
+
   removeHead() {
-    // v1 - nope
-    // const prevNode = this.head;
-    // if (prevNode.next === null) {
-    //   prevNode.next = prevNode.next.next;
-    // }
-    return this.head;
+    if (this.head.next === null) {
+      return this.head.value;
+    }
+    const temphead = this.head;
+    this.head = this.head.next;
+    return temphead.value;
   }
+
   contains(item) {
     let currNode = this.head;
-    console.log(currNode);
-    console.log('Next node:', currNode.next);
-    console.log('Item:', item);
-    console.log('currNode element:', currNode.value);
-    while (currNode.value !== item) {
+    while (currNode.next !== null) {
+      if (currNode.value === item) {
+        return true;
+      }
       currNode = currNode.next;
     }
-    return true;
+    return false;
   }
 }
 
