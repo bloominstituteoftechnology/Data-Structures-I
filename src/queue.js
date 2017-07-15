28783b1324294d52cb9c7cce25e4cf963e6c9a4b
Chooster <jcooleycoder@gmail.com>
2017-07-14 18:19:49 -0700

    Finished Queue

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,16 @@
 class Queue {
-
+  constructor() {
+    this.arr = [];
+  }
+  enqueue(element) {
+    this.arr.push(element);
+  }
+  dequeue() {
+    return this.arr.shift();
+  }
+  get size() {
+    return this.arr.length;
+  }
 }
 
 module.exports = Queue;
