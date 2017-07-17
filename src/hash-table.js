25902a1bf9862db553718a37c6ca9933d71eccdb
Allan Felipe <allan.murara@gmail.com>
2017-07-17 14:41:13 -0300

    Added hash-table with extra credits. incomplete till now

--- src/hash-table.js
+++ src/hash-table.js
@@ -1,11 +1,33 @@
-/* eslint-disable no-unused-vars */
 const { LimitedArray, getIndexBelowMax } = require('./hash-table-helpers');
 
 class HashTable {
   constructor() {
     this.limit = 8;
-    this.storage = new LimitedArray(this.limit);
-    // Do not modify anything inside of the constructor
+    this.table = new LimitedArray(this.limit);
+  }
+
+  insert(data, value) {
+    this.table.set(getIndexBelowMax(data, this.limit), value, data, (arg) => {
+      this.limit = this.table.limit;
+      return this.retrieve(arg, true);
+    });
+  }
+
+  remove(data) {
+    return this.limit;
+  }
+
+  retrieve(value, callbacked) {
+    const index = getIndexBelowMax(value, this.limit);
+    let position = 0;
+    if (Array.isArray(this.table.storage[index]) === true) {
+      for (let i = 0; i < value.length; i++) {
+        position += value.charCodeAt(i);
+      }
+      if (callbacked !== true) return this.table.storage[index][position] === undefined ? this.table.storage[index][0][1] : this.table.storage[index][position][1];
+      return { index, position };
+    }
+    return callbacked === true ? { index, position: -1 } : this.table.storage[index][1];
   }
 }
 
