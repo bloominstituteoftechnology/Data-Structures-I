c2e325cb1c76e662045c8388d011d39ba82f6005
Pratik Sampat <pratiks0809@gmail.com>
2017-07-17 23:17:02 -0500

    progress on linked-list.js

--- src/linked-list.js
+++ src/linked-list.js
@@ -4,13 +4,35 @@ class LinkedList {
     this.tail = null;
     // Do not modify anything inside of the constructor
   }
+  addToTail(value) {
+    if (this.head == null) {
+      this.tail = {
+        value,
+        next: null
+      };
+      this.head = this.tail;
+    } else {
+      this.tail = {
+        value,
+        next: null
+      };
+    }
+
+    console.log(this)
+  }
+  removeHead() {
+
+  }
+  contains() {
+
+  }
 }
 
 module.exports = LinkedList;
 
 
- // * Should have the methods: `addToTail`, `removeHead`, and `contains`.
- // * `addToTail` replaces the tail with a new value that is passed in.
- // * `removeHead` removes and returns the head node.
- // * `contains` should searth through the linked list and return true if a matching value is found.
- // * The `head` property is a reference to the first node and the `tail` property is a reference to the last node.  These are the only two properties that you need to keep track of an infinite number of nodes.  Build your nodes with objects.
+// * Should have the methods: `addToTail`, `removeHead`, and `contains`.
+// * `addToTail` replaces the tail with a new value that is passed in.
+// * `removeHead` removes and returns the head node.
+// * `contains` should searth through the linked list and return true if a matching value is found.
+// * The `head` property is a reference to the first node and the `tail` property is a reference to the last node.  These are the only two properties that you need to keep track of an infinite number of nodes.  Build your nodes with objects.
