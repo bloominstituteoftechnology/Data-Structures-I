2b576f2754597e3814bcbdb3d7d1b9346a8c27c0
Sandeep Chandran <deepsun80@yahoo.com>
2017-07-17 23:09:28 -0500

    Update Files

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,16 @@
 class Queue {
-
+  constructor() {
+    this.queue = [];
+  }
+  enqueue(value) {
+    this.queue.push(value);
+  }
+  dequeue() {
+    return this.queue.shift();
+  }
+  get size() {
+    return this.queue.length;
+  }
 }
 
 module.exports = Queue;
--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,16 @@
 class Stack {
-
+  constructor() {
+    this.stack = [];
+  }
+  add(value) {
+    this.stack.push(value);
+  }
+  remove() {
+    return this.stack.pop();
+  }
+  get size() {
+    return this.stack.length;
+  }
 }
 
 module.exports = Stack;
