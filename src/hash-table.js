a3128d7acde785c41320e7cbbdd44c61f6228a20
Sandeep Chandran <deepsun80@yahoo.com>
2017-07-19 11:17:43 -0500

    Updated files

--- src/hash-table.js
+++ src/hash-table.js
@@ -7,6 +7,39 @@ class HashTable {
     this.storage = new LimitedArray(this.limit);
     // Do not modify anything inside of the constructor
   }
+  insert(key, value) {
+    const pairArray = [[key, value]];
+    const index = getIndexBelowMax(key, this.limit);
+    let bucket = this.storage.get(index);
+    if (!bucket) {
+      this.storage.set(index, pairArray);
+      bucket = this.storage.get(index);
+    } else {
+      for (let i = 0; i < bucket.length; i++) {
+        if (bucket[i][0] === key) {
+          bucket[i][1] = value;
+        }
+      }
+      bucket.push(pairArray.pop());
+    }
+  }
+  retrieve(key) {
+    const index = getIndexBelowMax(key, this.limit);
+    const bucket = this.storage.get(index);
+    if (!bucket) return undefined;
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) return bucket[i][1];
+    }
+  }
+  remove(key) {
+    const index = getIndexBelowMax(key, this.limit);
+    const bucket = this.storage.get(index);
+    if (bucket) {
+      for (let i = 0; i < bucket.length; i++) {
+        if (bucket[i][0] === key) bucket.splice([i][1], 1);
+      }
+    }
+  }
 }
 
 module.exports = HashTable;
--- src/linked-list.js
+++ src/linked-list.js
@@ -4,6 +4,40 @@ class LinkedList {
     this.tail = null;
     // Do not modify anything inside of the constructor
   }
+  addToTail(input) {
+    const node = {
+      value: input,
+      next: null
+    };
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+  }
+  removeHead() {
+    const headObj = this.head.value;
+    if (this.head !== null) {
+      this.head = this.head.next;
+    }
+    return headObj;
+  }
+  contains(input) {
+    const cb = (node) => {
+      if (node.value === input) return true;
+      return false;
+    };
+    if (this.head.value === input) return true;
+    let current = this.head.next;
+    let bool = false;
+    while (current.next !== null) {
+      bool = cb(current);
+      current = current.next;
+    }
+    return bool;
+  }
 }
 
 module.exports = LinkedList;
