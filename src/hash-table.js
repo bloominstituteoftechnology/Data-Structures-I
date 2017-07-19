74f54771e339bd2c9a14053f73c4eec907a91169
Matt Higbee <mhigbee@hotmail.com>
2017-07-18 22:24:16 -0600

    saving progress on collisions

--- src/hash-table.js
+++ src/hash-table.js
@@ -7,13 +7,34 @@ class HashTable {
     this.storage = new LimitedArray(this.limit);
     // Do not modify anything inside of the constructor
   }
-  // insert(key, value) {
-  //   const hash = this.getHash(key);
-  //   if (this.storage.get(hash) !== undefined) {  
-  //   } else {
-  //     this.storage.set(this.getHash(key), value);
-  //   }
-  // }
+  
+  insert(key, value) {
+    const hash = this.getHash(key);
+    if (this.storage.get(hash) !== undefined) {
+      //copy whatever is that the location already
+      const backup = this.storage.get(hash);
+      // remove whatever was in that position
+      remove(hash);
+      // set a bucket
+      const bucket = [];
+      for (var i = 0; i < bucket.length; i++) {
+        var tuple = bucket[i];
+        if (tuple[0] === hash) {
+          tuple[1] = value;
+          return;
+        };
+      };
+      // replace whatever was in that position
+      
+      // Like this?[[a,z],[b],[c]]
+      // create 2 d array at that position
+
+      
+      
+    } else {
+    this.storage.set(this.getHash(key), value);
+    }
+  }
 
   getHash(key) {
     if (typeof key === 'number' && Number.isInteger(key) && key < this.limit) {
@@ -24,7 +45,7 @@ class HashTable {
   }
 
   remove(key) {
-    this.insert(key, undefined);
+    this.storage.set(this.getHash(key), undefined);
   }
 
   retrieve(key) {
@@ -33,7 +54,6 @@ class HashTable {
 }
 
 module.exports = HashTable;
-
 
 // Should have the methods: insert, remove, and retrieve.
 // insert should take a key value pair and add the value to the hash table.
