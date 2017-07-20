7fe271e42de36987b31bddcf131f78ab04bce6d5
Sandeep Chandran <deepsun80@yahoo.com>
2017-07-19 21:40:41 -0500

    Updated Files

--- src/hash-table.js
+++ src/hash-table.js
@@ -6,21 +6,29 @@ class HashTable {
     this.limit = 8;
     this.storage = new LimitedArray(this.limit);
     // Do not modify anything inside of the constructor
+    this.size = 0;
+  }
+  checkSize() {
+    if (this.size >= 6) this.limit = 16;
   }
   insert(key, value) {
+    this.checkSize();
     const pairArray = [[key, value]];
     const index = getIndexBelowMax(key, this.limit);
     let bucket = this.storage.get(index);
     if (!bucket) {
       this.storage.set(index, pairArray);
       bucket = this.storage.get(index);
+      this.size ++;
     } else {
       for (let i = 0; i < bucket.length; i++) {
         if (bucket[i][0] === key) {
           bucket[i][1] = value;
+          this.size ++;
         }
       }
       bucket.push(pairArray.pop());
+      this.size ++;
     }
   }
   retrieve(key) {
