138fd5285b7ff72f838dcc643300b02ac125fdb7
Colin Gage <colin.m.gage@gmail.com>
2017-07-19 20:31:28 -0700

    Added comments and skeleton of functions to hash-table.js.

--- src/hash-table.js
+++ src/hash-table.js
@@ -7,6 +7,19 @@ class HashTable {
     this.storage = new LimitedArray(this.limit);
     // Do not modify anything inside of the constructor
   }
+  insert(key, value) {
+  	// Takes a key value pair and adds the value to the hash table
+  }
+  retrieve(key) {
+  	// Returns the value associated with the key passed into the method as the argument
+  }
+  remove(key) {
+  	// Removes the value associated with the key passed from the hash table
+  }
 }
+
+// Should properly handle collisions.  If two keys map to the same index in the storage table
+// then you should store a 2d array as the value.  Make each key/value pair its own array that
+// is nested inside of the array stored at that index on the table.
 
 module.exports = HashTable;
