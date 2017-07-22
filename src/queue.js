4fafbb40183fb67bc142203b05fb97a1c1a01f58
Vance Villanueva <vancervillanueva@gmail.com>
2017-07-22 05:23:41 -0700

    Data Structures I with Bridgette

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,16 @@
 class Queue {
-
+  constructor() {
+    this.storage = [];
+  }
+  enqueue(x) {
+    return this.storage.push(x);
+  }
+  dequeue(y) {
+    return this.storage.shift(y);
+  }
+  get size() {
+    return this.storage.length;
+  }
 }
 
 module.exports = Queue;
--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,17 @@
 class Stack {
-
+  constructor() {
+    this.storage = [];
+  }
+  add(x) {
+    return this.storage.push(x);
+  }
+  remove(y) {
+    return this.storage.pop(y);
+  }
+  get size() {
+    return this.storage.length;
+  }
 }
+
 
 module.exports = Stack;
