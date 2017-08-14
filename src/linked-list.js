4aec270cf880e09c761c006656e42f5996aa633e
Adam Lower <adamlowerj@gmail.com>
2017-08-14 18:33:34 -0400

    Finished linked-list.js

--- src/linked-list.js
+++ src/linked-list.js
@@ -4,6 +4,34 @@ class LinkedList {
     this.tail = null;
     // Do not modify anything inside of the constructor
   }
+  addToTail(value) {
+    const node = { value, next: null };
+
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+    }
+    this.tail = node;
+  }
+
+  removeHead() {
+    if (this.head) {
+      const value = this.head.value;
+      this.head = this.head.next;
+      return value;
+    }
+  }
+
+  contains(value) {
+    let current = this.head;
+
+    while (current) {
+      if (current.value === value) return true;
+      current = current.next;
+    }
+    return false;
+  }
 }
 
 module.exports = LinkedList;
