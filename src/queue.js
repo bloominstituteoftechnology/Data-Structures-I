02855751d28445474956c7e90874873a1eda2bb1
Raymond <nomyards@gmail.com>
2017-08-14 18:16:05 -0400

    Complete queue work

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,17 @@
 class Queue {
-
+  constructor() {
+    this.queue = [];
+  }
+  enqueue(item) {
+    this.queue.push(item);
+  }
+  dequeue() {
+    return this.queue.shift();
+  }
+  get size() {
+    return this.queue.length;
+  }
 }
+
 
 module.exports = Queue;
