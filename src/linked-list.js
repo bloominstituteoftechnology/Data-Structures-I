b0f8c367bc027ac17baad6a7a2d6d7beee51a381
Raymond <nomyards@gmail.com>
2017-08-14 19:24:42 -0400

    Complete linked-list

--- src/linked-list.js
+++ src/linked-list.js
@@ -4,6 +4,35 @@ class LinkedList {
     this.tail = null;
     // Do not modify anything inside of the constructor
   }
+  addToTail(value) {
+    const node = {
+      value,
+      next: null
+    };
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+    }
+    this.tail = node;
+  }
+  removeHead(value) {
+    if (this.head) {
+      const origHead = this.head.value;
+      this.head = this.head.next;
+      return origHead;
+    }
+  }
+  contains(value) {
+    let currentNode = this.head;
+    while (currentNode) {
+      if (currentNode.value === value) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
+  }
 }
 
 module.exports = LinkedList;
