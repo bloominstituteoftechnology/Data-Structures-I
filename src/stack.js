c084e8b47cc71e5307ca97f8ddff583ea663bf45
Tyler <blackbelt@yahoo.com>
2017-08-14 18:08:13 -0400

    finished stack.js

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,21 @@
 class Stack {
+  constructor() {
+    this.storage = [];
+  }
+
+  add(value) {
+    return this.storage.push(value);
+  }
+
+  remove() {
+    return this.storage.pop();
+  }
+
+  get size() {
+    return this.storage.length;
+  }
 
 }
 
 module.exports = Stack;
+
