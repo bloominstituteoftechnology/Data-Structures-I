ce32e420ca920796fd65f8dfc518cf9488854edc
Dylan <dylan@Dylans-MacBook.local>
2017-08-14 18:06:37 -0400

    completed stack

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,21 @@
 class Stack {
-
+  constructor() {
+    this.stack = [];
+    this.count = 0;
+  }
+  add(value) {
+    this.stack.push(value);
+    this.count++;
+  }
+  remove() {
+    if (this.count) {
+      this.count--;
+      return this.stack.pop();
+    }
+  }
+  get size() {
+    return this.count;
+  }
 }
 
 module.exports = Stack;
