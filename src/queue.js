5d640e54b916acffc31d4e14d86237705a480821
phytertek <ryan.phytertek@gmail.com>
2017-08-14 16:40:12 -0400

    finish solution to queue.js

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,26 @@
-class Queue {
+/**
+ * #### Queues
 
+ * Should have the methods: `enqueue`, `dequeue`, and a getter for the property `size`
+ * `enqueue` should add an item to the back of the queue.
+ * `dequeue` should remove an item from the front of the queue.
+ * `size` should return the number of items in the queue.
+ */
+
+
+class Queue {
+  constructor() {
+    this.queue = [];
+  }
+  enqueue(item) {
+    this.queue.push(item)
+  }
+  dequeue() {
+    return this.queue.shift()
+  }
+  get size() {
+    return this.queue.length;
+  }
 }
 
 module.exports = Queue;
