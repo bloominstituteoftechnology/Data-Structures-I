b73d7b113f1f4c4e7cdead0f9f08039986ab2ce7
phytertek <ryan.phytertek@gmail.com>
2017-08-14 16:56:31 -0400

    finish solution to stack.js

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,25 @@
-class Stack {
+/**
+ * #### Stacks
 
+ * Should have the methods: `add`, `remove`, and a getter for the property `size`
+ * `add` should accept a value and place it on top of the stack.
+ * `remove` should remove and return the top value off of the stack.
+ * `size` should return how many items are on the stack.
+ */
+
+class Stack {
+  constructor() {
+    this.stack = [];
+  }
+  add(item) {
+    this.stack.unshift(item);
+  }
+  remove() {
+    return this.stack.shift();
+  }
+  get size() {
+    return this.stack.length;
+  }
 }
 
 module.exports = Stack;
