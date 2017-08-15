35d3b1254d3f6e2edf80b6db576c0d711029e097
Frank Faustino <frank@Franks-MacBook-Pro.local>
2017-08-14 17:14:54 -0700

    Completed

--- src/linked-list.js
+++ src/linked-list.js
@@ -4,6 +4,41 @@ class LinkedList {
     this.tail = null;
     // Do not modify anything inside of the constructor
   }
+  addToTail(node) {
+    if (this.tail === null && this.head === null) {
+      this.tail = {};
+      this.head = {};
+      this.tail.contents = [node];
+      this.tail.value = this.tail.contents[this.tail.contents.length - 1];
+      this.head.value = this.tail.contents[0];
+    } else {
+      this.tail.contents.push(node);
+      this.tail.value = this.tail.contents[this.tail.contents.length - 1];
+    }
+  }
+  removeHead() {
+    const temp = this.head.value;
+    this.tail.contents.shift();
+    this.head.value = this.tail.contents[0];
+    return temp;
+  }
+  contains(x) {
+    return this.tail.contents.includes(x);
+  }
 }
 
 module.exports = LinkedList;
+
+// const newObj = new LinkedList();
+// newObj.addToTail(1);
+// newObj.addToTail(2);
+// newObj.addToTail(3);
+// newObj.addToTail(4);
+// newObj.addToTail(5);
+// newObj.addToTail('hello')
+// console.log(newObj.removeHead())
+// console.log(newObj.removeHead())
+// console.log(newObj.removeHead())
+// console.log(newObj.removeHead())
+// console.log(newObj.removeHead())
+// console.log(`contents: ${newObj.tail.contents}`);
--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,27 @@
 class Stack {
-
+  constructor(options) {
+    this.newArr = [];
+  }
+  add(value) {
+    return this.newArr.push(value);
+  }
+  remove() {
+    return this.newArr.pop();
+  }
+  get size() {
+    return this.newArr.length;
+  }
 }
 
 module.exports = Stack;
+
+// const newObj = new Stack()
+// newObj.add(77)
+// newObj.add(null)
+// newObj.add(null)
+// newObj.add(null)
+// newObj.add(null)
+// newObj.remove()
+// const a = newObj.size()
+// // console.log(a);
+// // console.log(newObj.newArr)
