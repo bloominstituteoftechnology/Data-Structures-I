49b3878cdeef2c9715d50c474417959a84464c36
phytertek <ryan.phytertek@gmail.com>
2017-08-14 21:18:59 -0400

    finish solution for linked-list.js

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,8 +1,90 @@
+/**
+ * #### Linked Lists
+
+ * Should have the methods: `addToTail`, `removeHead`, and `contains`.
+ * `addToTail` replaces the tail with a new value that is passed in.
+ * `removeHead` removes and returns the head node.
+ * `contains` should searth through the linked list and return true if a matching value is found.
+ * The `head` property is a reference to the first node and the `tail` property is a reference to
+ * the last node.  These are the only two properties that you need to keep track of an infinite
+ * number of nodes.  Build your nodes with objects.
+ */
+
 class LinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
     // Do not modify anything inside of the constructor
+  }
+  // addToTail(value) {
+  //   const node = {
+  //     value,
+  //     next: null
+  //   };
+  //   if (!this.head) {
+  //     this.head = node;
+  //     this.tail = node;
+  //   } else if (!this.head.next) {
+  //     this.head.next = node;
+  //     this.tail = node;
+  //   } else {
+  //     this.tail.next= node;
+  //     this.tail = node
+  //   }
+  // }
+  // removeHead() {
+  //   if (!this.head) return null;
+  //   if (!this.head.next) {
+  //     this.head = null;
+  //     this.tail = null;
+  //     return null;
+  //   } else {
+  //     const returnVal = this.head.value;
+  //     this.head = this.head.next;
+  //     return returnVal;
+  //   }
+  // }
+  // contains(value) {}
+  addToTail(value) {
+    const node = {
+      next: null,
+      value
+    };
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+      return;
+    }
+    if (this.head.next === null) {
+      this.head.next = node;
+      this.tail = node;
+      return;
+    }
+    this.tail.next = node;
+    this.tail = node;
+  }
+
+  removeHead() {
+    if (!this.head) return null;
+    if (!this.head.next) {
+      const head = this.head;
+      this.head = null;
+      this.tail = null;
+      return head.value;
+    }
+    const returnVal = this.head.value;
+    this.head = this.head.next;
+    return returnVal;
+  }
+
+  contains(value) {
+    if (this.head === null) return false;
+    const findValue = (node) => {
+      if (node.next === null) return false;
+      else if (node.value === value) return true;
+      return findValue(node.next);
+    };
+    return findValue(this.head);
   }
 }
 
