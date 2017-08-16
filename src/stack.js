f08e7b64c50f2f2f6f24e29a47590376a97e2871
Frank Faustino <frank@Franks-MacBook-Pro.local>
2017-08-16 12:21:55 -0700

    Completed stack.js

--- src/stack.js
+++ src/stack.js
@@ -1,27 +1,16 @@
 class Stack {
-  constructor(options) {
-    this.newArr = [];
+  constructor() {
+    this.storage = [];
   }
   add(value) {
-    return this.newArr.push(value);
+    this.storage.push(value);
   }
   remove() {
-    return this.newArr.pop();
+    return this.storage.pop();
   }
   get size() {
-    return this.newArr.length;
+    return this.storage.length;
   }
 }
 
 module.exports = Stack;
-
-// const newObj = new Stack()
-// newObj.add(77)
-// newObj.add(null)
-// newObj.add(null)
-// newObj.add(null)
-// newObj.add(null)
-// newObj.remove()
-// const a = newObj.size()
-// // console.log(a);
-// // console.log(newObj.newArr)
