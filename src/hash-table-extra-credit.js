86bc3c304b2f03f64df3b5884dbc88b48e28d1ab
phytertek <ryan.phytertek@gmail.com>
2017-08-16 00:19:50 -0400

    Complete extra credit for hash-table-extra-credit.js (includes test)

---
+++ src/hash-table-extra-credit.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-unused-vars */
+
+/**
+ * #### Hash Tables
+ * ### Extra Credit
+ * Uncomment the final test in `hash-table.test.js` and make the hash-table rebalance.
+ * As a hash table increases in size the associated storage table will typically double
+ * in size once it reaches a certain capacity. Change the hash table so that it doubles
+ * the size of the storage table once it is 75% full.
+ */
+const { LimitedArray, getIndexBelowMax } = require('./hash-table-helpers');
+
+class HashTable {
+  constructor() {
+    this.limit = 8;
+    this.storage = new LimitedArray(this.limit);
+    // Do not modify anything inside of the constructor
+  }
+
+  manageStorage() {
+    if (this.storage.length / this.limit > 0.75) {
+      this.limit *= 2;
+      const resizedStorage = new LimitedArray(this.limit);
+      this.storage.each((element, index) => {
+        resizedStorage.set(index, element);
+      });
+      this.storage = resizedStorage;
+    }
+  }
+
+  getIndex(key) {
+    return getIndexBelowMax(`${key}`, this.limit);
+  }
+
+  insert(key, value) {
+    this.manageStorage();
+    const index = this.getIndex(key);
+    const element = this.storage.get(index) || [];
+    element.push([key, value]);
+    this.storage.set(index, element);
+  }
+
+  retrieve(key) {
+    const index = this.getIndex(key);
+    const element = this.storage.get(index);
+    if (!element) return;
+    const values = element.find(value => value[0] === key);
+    const returnVal = values ? values[1] : undefined;
+    return returnVal;
+  }
+
+  remove(key) {
+    const index = this.getIndex(key);
+    const element = this.storage.get(index);
+    if (element.length === 1) return this.storage.set(index, undefined);
+    element.forEach((value, i) => {
+      if (value[0] === key) element.splice(i, 1);
+      this.storage.set(index, element);
+    });
+  }
+}
+
+module.exports = HashTable;
+
---
+++ tests/hash-table-extra-credit.test.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-undef, no-prototype-builtins */
+const HashTable = require('../src/hash-table-extra-credit');
+
+let hashTable;
+
+describe('HashTable', () => {
+  beforeEach(() => {
+    hashTable = new HashTable();
+  });
+
+  it('should have "insert", "remove" and "retrieve" methods', () => {
+    const hasInsert = Object.getPrototypeOf(hashTable).hasOwnProperty('insert');
+    const hasRemove = Object.getPrototypeOf(hashTable).hasOwnProperty('remove');
+    const hasRetrieve = Object.getPrototypeOf(hashTable).hasOwnProperty('retrieve');
+    expect(hasInsert).toBe(true);
+    expect(hasRemove).toBe(true);
+    expect(hasRetrieve).toBe(true);
+  });
+
+  it('should save and return values by key', () => {
+    hashTable.insert('hello', 'there');
+    expect(hashTable.retrieve('hello')).toBe('there');
+  });
+
+  it('should properly remove items', () => {
+    hashTable.insert('Ben', 'Nelson');
+    hashTable.remove('Ben');
+    // console.log(hashTable.5)
+    expect(hashTable.retrieve('Ben')).toBe(undefined);
+  });
+
+  it('should handle numbers as keys', () => {
+    hashTable.insert(0, 'First Value');
+    hashTable.insert(1, 'Second Value');
+    expect(hashTable.retrieve(0)).toBe('First Value');
+    expect(hashTable.retrieve(1)).toBe('Second Value');
+  });
+
+  // it('should overwrite the value when using the same key', () => {
+  //   hashTable.insert(0, 'First Value');
+  //   hashTable.insert(0, 'Second Value');
+  //   expect(hashTable.retrieve(0)).toBe('Second Value');
+  // });
+
+  /* Extra Credit */
+
+  it('should properly handle collisions', () => {
+    hashTable.insert('B', 'First Value');
+    hashTable.insert('HI!', 'Second Value');
+    expect(hashTable.retrieve('B')).toBe('First Value');
+    expect(hashTable.retrieve('HI!')).toBe('Second Value');
+  });
+  it('should resize the hash table when > 75% full', () => {
+    hashTable.insert('a', true);
+    hashTable.insert('b', true);
+    hashTable.insert('c', true);
+    hashTable.insert('d', true);
+    hashTable.insert('e', true);
+    hashTable.insert('f', true);
+    hashTable.insert('g', true);
+    expect(hashTable.limit).toBe(16);
+    expect(hashTable.storage.length).toBe(8);
+  });
+});
