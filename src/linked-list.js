def4eb8ecc7d0751d075065fbdc348860f0a9c88
Jacob Stewart <cool_stewj@yahoo.com>
2017-09-01 21:37:06 -0500

    Finished for now, hash table can wait for class, looks like you guys teach it well.

--- src/linked-list.js
+++ src/linked-list.js
@@ -4,7 +4,47 @@ class LinkedList {
     this.tail = null;
     // Do not modify anything inside of the constructor
   }
-  
+
+  addToTail(value) {
+    if (!this.head) {
+      // it's the first item
+      this.head = {
+        value,
+        next: null
+      };
+      this.tail = this.head;
+    } else {
+      // put it at the end
+      this.tail.next = {
+        value,
+        next: null
+      };
+      this.tail = this.tail.next;
+    }
+  }
+
+  removeHead() {
+    if (!this.head) {
+      return undefined;
+    }
+    const value = this.head.value;
+    this.head = this.head.next;
+    return value;
+  }
+
+  contains(value) {
+    if (!this.head) {
+      return false;
+    }
+    let iter = this.head;
+    while (iter.next !== null) {
+      if (iter.value === value) {
+        return true;
+      }
+      iter = iter.next;
+    }
+    return false;
+  }
 }
 
 module.exports = LinkedList;
--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,49 @@
 class Queue {
+  constructor() {
+    this.size = 0;
+    this.head = null;
+  }
 
+  enqueue(val) {
+    if (!this.head) {
+      // it's the first item
+      this.head = {
+        val,
+        next: null
+      };
+    } else {
+      // put it at the front
+      const newhead = {
+        val,
+        next: this.head
+      };
+      this.head = newhead;
+    }
+    this.size++;
+  }
+
+  dequeue() {
+    if (!this.head) {
+      return undefined;
+    } else if (!this.head.next) {
+      const val = this.head.val;
+      this.head = null;
+      this.size--;
+      return val;
+    }
+    let beforelast = this.head;
+    while (beforelast.next.next !== null) {
+      beforelast = beforelast.next;
+    }
+    const endoflistval = beforelast.next.val;
+    beforelast.next = null;
+    this.size--;
+    return endoflistval;
+  }
+
+  size() {
+    return this.size;
+  }
 }
 
 module.exports = Queue;
