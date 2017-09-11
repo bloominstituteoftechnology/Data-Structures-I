1a2d91d0bc4bd1866d86f51e4145493634c14106
Jacob Stewart <cool_stewj@yahoo.com>
2017-09-11 17:44:21 -0500

    Will do extra credit later

--- src/hash-table.js
+++ src/hash-table.js
@@ -7,6 +7,44 @@ class HashTable {
     this.storage = new LimitedArray(this.limit);
     // Do not modify anything inside of the constructor
   }
+
+  insert(key, val) {
+    const index = getIndexBelowMax(key, this.limit);
+    const bucket = this.storage.get(index);
+    if (bucket) {
+      for (let i = 0; i < bucket.length; i++) {
+        if (bucket[i][0] === key) {
+          bucket[i][1] = val;
+        }
+      }
+      bucket.push([key, val]);
+      this.storage.set(index, bucket);
+    } else {
+      this.storage.set(index, [[key, val]]);
+    }
+  }
+
+  retrieve(key) {
+    const index = getIndexBelowMax(key, this.limit);
+    const bucket = this.storage.get(index);
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        return bucket[i][1];
+      }
+    }
+    return undefined;
+  }
+
+  remove(key) {
+    const index = getIndexBelowMax(key, this.limit);
+    const bucket = this.storage.get(index);
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        bucket.splice(i, 1);
+        this.storage.set(index, bucket);
+      }
+    }
+  }
 }
 
 module.exports = HashTable;
