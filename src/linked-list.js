f535b0d50656da6e12679a82202eac9a2170497a
Nick Costigan <nick.costigan@gmail.com>
2017-09-11 20:17:09 -0400

    finished linked list

--- src/linked-list.js
+++ src/linked-list.js
@@ -4,14 +4,35 @@ class LinkedList {
     this.tail = null;
     // Do not modify anything inside of the constructor
   }
-  addToTail() {
-
+  addToTail(value) {
+    if (this.head === null) {
+      this.head = {
+        value,
+        next: null
+      };
+      this.tail = this.head;
+    } else {
+      this.tail.next = {
+        value,
+        next: null
+      };
+      this.tail = this.tail.next;
+    }
   }
   removeHead() {
-
+    const value = this.head.value;
+    this.head = this.head.next;
+    return value;
   }
-  contains() {
-
+  contains(value) {
+    let node = this.head;
+    while (node.next !== null) {
+      if (node.value === value) {
+        return true;
+      }
+      node = node.next;
+    }
+    return false;
   }
 }
 
