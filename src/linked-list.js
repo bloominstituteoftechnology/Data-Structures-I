a92d7879cbd2d1ca1cdaaa7fd8e3fc07fa4b5de2
Darryl Green <darryl.alexander.green@gmail.com>
2017-09-11 19:20:35 -0500

    All tests passed

--- src/linked-list.js
+++ src/linked-list.js
@@ -4,7 +4,34 @@ class LinkedList {
     this.tail = null;
     // Do not modify anything inside of the constructor
   }
-  
+  addToTail(item) {
+    if (this.tail === null) {
+      this.tail = {
+        next: null,
+        value: item
+      };
+      this.head = this.tail;
+    } else {
+      this.tail.next = {
+        next: null,
+        value: item
+      };
+      this.tail = this.tail.next;
+    }
+  }
+  removeHead() {
+    const temp = this.head;
+    this.head = this.head.next;
+    return temp.value;
+  }
+  contains(item) {
+    let index = this.head;
+    while (index.next !== null) {
+      if (index.value === item) {
+        return true;
+      } index = index.next;
+    } return false;
+  }
 }
 
 module.exports = LinkedList;
--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,17 @@
 class Stack {
+  constructor() {
+    this.arr = [];
+  }
+  push(item) {
+    this.arr.push(item);
+  }
+  pop() {
+    return this.arr.pop();
+  }
 
+  get size() {
+    return this.arr.length;
+  }
 }
 
 module.exports = Stack;
