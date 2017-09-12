af749f0924b9f5ad17a8e0c49c8d11524b427a88
Joe Roland <joroland@gmail.com>
2017-09-12 19:58:01 -0400

    Completed Data Structures I

--- src/hash-table.js
+++ src/hash-table.js
@@ -1,13 +1,105 @@
 /* eslint-disable no-unused-vars */
 const { LimitedArray, getIndexBelowMax } = require('./hash-table-helpers');
 
+// class HashTable {
+//   constructor() {
+//     this.limit = 8;
+//     this.storage = new LimitedArray(this.limit);
+//     // Do not modify anything inside of the constructor
+//   }
+
+//   insert(key, val) {
+//     if (this.storage.length > (this.limit * 0.75)) {
+//       this.limit *= 2;
+//       this.storage.limit = this.limit;
+//     }
+//     const index = getIndexBelowMax(key, this.limit);
+//     let bucket = this.storage.get(index);
+
+//     if (!bucket) {
+//       bucket = [];
+//     } else {
+//       bucket.forEach((elem) => {
+//         if (elem[0] === key) {
+//           elem[1] = val;
+//         }
+//       });
+//     }
+//     bucket.push([key, val]);
+//     this.storage.set(index, bucket);
+//     return undefined;
+//   }
+
+//   retrieve(key) {
+//     const index = getIndexBelowMax(key, this.limit);
+//     const bucket = this.storage.get(index);
+
+//     if (!bucket) return undefined;
+
+//     for (let i = 0; i < bucket.length; i++) {
+//       if (bucket[i][0] === key) return bucket[i][1];
+//     }
+//   }
+
+//   remove(key) {
+//     const index = getIndexBelowMax(key, this.limit);
+//     const bucket = this.storage.get(index);
+//     bucket.forEach((elem) => {
+//       if (elem[0] === key) {
+//         elem.shift();
+//       }
+//     });
+//     this.storage.set(index, bucket);
+//     return undefined;
+//   }
+// }
+
+/* ******* LINKED LIST VERSION ******* */
+
+const LinkedList = require('./linked-list');
+
 class HashTable {
   constructor() {
     this.limit = 8;
     this.storage = new LimitedArray(this.limit);
-    // Do not modify anything inside of the constructor
   }
-  
+
+  insert(key, val) {
+    if (this.storage.length > (this.limit * 0.75)) {
+      this.limit *= 2;
+      this.storage.limit = this.limit;
+    }
+
+    const index = getIndexBelowMax(key, this.limit);
+    let bucket = this.storage.get(index);
+
+    if (!bucket) {
+      bucket = new LinkedList();
+    }
+    if (!bucket.updateNode(key, val)) {
+      bucket.addToTail(val, key);
+    }
+    this.storage.set(index, bucket);
+    return undefined;
+  }
+
+  retrieve(key) {
+    const index = getIndexBelowMax(key, this.limit);
+    const bucket = this.storage.get(index);
+
+    if (!bucket) return undefined;
+
+    return bucket.find(key);
+  }
+
+  remove(key) {
+    const index = getIndexBelowMax(key, this.limit);
+    const bucket = this.storage.get(index);
+
+    bucket.deleteNode(key);
+    this.storage.set(index, bucket);
+    return undefined;
+  }
 }
 
 module.exports = HashTable;
--- src/linked-list.js
+++ src/linked-list.js
@@ -4,7 +4,122 @@ class LinkedList {
     this.tail = null;
     // Do not modify anything inside of the constructor
   }
-  
+
+  addToTail(value, key) {
+    const newNode = {
+      key,
+      value,
+      prev: null,
+      next: null
+    };
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  removeHead() {
+    if (!this.head) return undefined;
+    const val = this.head.value;
+    this.head = this.head.next;
+    if (this.head) {
+      this.head.prev = null;
+    } else {
+      this.tail = null;
+    }
+    return val;
+  }
+
+  contains(val) {
+    let current = this.head;
+
+    while (current.value !== val) {
+      current = current.next;
+      if (!current.next) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // Extra methods to help implement Stack, Queue, and Hash Table
+
+  removeTail() {
+    if (!this.tail) return undefined;
+    const val = this.tail.value;
+    this.tail = this.tail.prev;
+    if (this.tail) {
+      this.tail.next = null;
+    } else {
+      this.head.next = null;
+    }
+    return val;
+  }
+
+  find(key) {
+    let current = this.head;
+
+    while (current) {
+      if (current.key === key) {
+        return current.value;
+      }
+      current = current.next;
+    }
+
+    return undefined;
+  }
+
+  deleteNode(key) {
+    let current = this.head;
+
+    while (current) {
+      if (current.key === key) {
+        if (current === this.head && current === this.tail) {
+          this.head = null;
+          this.tail = null;
+        } else if (current === this.head) {
+          this.head = this.head.next;
+          this.head.prev = null;
+        } else if (current === this.tail) {
+          this.tail = this.tail.prev;
+          this.tail.next = null;
+        } else {
+          current.prev.next = current.next;
+          current.next.prev = current.prev;
+        }
+      }
+      current = current.next;
+    }
+    return undefined;
+  }
+
+  updateNode(key, val) {
+    let current = this.head;
+
+    while (current) {
+      if (current.key === key) {
+        current.value = val;
+      }
+      current = current.next;
+    }
+    return undefined;
+  }
+
+  get size() {
+    const recursiveSize = (node) => {
+      if (!node) return 0;
+
+      return recursiveSize(node.next) + 1;
+    };
+
+    const currentNode = this.head;
+    const size = recursiveSize(currentNode, 0);
+    return size;
+  }
 }
 
 module.exports = LinkedList;
