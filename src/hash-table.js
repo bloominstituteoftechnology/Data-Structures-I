80c4ff873cedde9f21b883e08d82810aed2d1952
Samer Dabra <samer.dabra@googlemail.com>
2017-09-13 00:20:57 +0100

    Complete Data Structures w/o Extra Credit

--- src/hash-table.js
+++ src/hash-table.js
@@ -7,7 +7,38 @@ class HashTable {
     this.storage = new LimitedArray(this.limit);
     // Do not modify anything inside of the constructor
   }
-  
+  insert(key, value) {
+    const index = getIndexBelowMax(key, this.limit);
+    if (this.storage[index] === undefined) {
+      this.storage[index] = [[key, value]];
+    } else {
+      this.storage[index].push([key, value]);
+    }
+  }
+  retrieve(key) {
+    const index = getIndexBelowMax(key, this.limit);
+    if (this.storage[index] === undefined) {
+      return false;
+    }
+    let valueIn2DArray = null;
+    this.storage[index].forEach((element) => {
+      if (key === element[0]) {
+        valueIn2DArray = element[1];
+      }
+    });
+    return valueIn2DArray;
+  }
+  remove(key) {
+    const index = getIndexBelowMax(key, this.limit);
+    if (this.storage[index] === undefined) {
+      return undefined;
+    }
+    this.storage[index].forEach((element) => {
+      if (key === element[0]) {
+        element[1] = undefined;
+      }
+    });
+  }
 }
 
 module.exports = HashTable;
