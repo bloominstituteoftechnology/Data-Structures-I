495aa792efecd11a6663eee029e3477d4c4aea92
analogy <actweed47@gmail.com>
2017-09-13 15:32:23 -0400

    Changed code to Data Structures

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,18 @@
 class Queue {
+  constructor() {
+    this.storage = [];
+  }
 
+  enqueue(any) {
+    this.storage.push(any);
+  }
+
+  dequeue() {
+    return this.storage.shift();
+  }
+  get size() {
+    return this.storage.length;
+  }
 }
 
 module.exports = Queue;
--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,19 @@
 class Stack {
+  constructor() {
+    this.storage = [];  //  enables each instance of stack to have its own container
+  }
 
+  push(node) {
+    this.storage.push(node);
+  }
+
+  pop() {
+    return this.storage.pop();
+  }
+  get size() {
+    return this.storage.length;
+  }
 }
 
 module.exports = Stack;
+
