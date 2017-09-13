ef92d99965b7851e6f0561cb1ea420f4e0714714
Tai N <ndhuutai1@gmail.com>
2017-09-13 12:58:43 -0700

    still trying to figure out how to handle collision, linter is difficult to debug in

--- src/hash-table.js
+++ src/hash-table.js
@@ -7,7 +7,36 @@ class HashTable {
     this.storage = new LimitedArray(this.limit);
     // Do not modify anything inside of the constructor
   }
-  
+  insert(key, value) {
+    if (typeof key === 'number') {
+      this.storage.set(key, value);
+    }
+    if (typeof key === 'string') {
+      key = key.toLowerCase();
+      const position = getIndexBelowMax(key, this.limit);
+      this.storage.set(position, value);
+    }
+  }
+  remove(key) {
+    if (typeof key === 'number') {
+      return this.storage.set(key, undefined);
+    }
+    if (typeof key === 'string') {
+      key = key.toLowerCase();
+    }
+    const position = getIndexBelowMax(key, this.limit);
+    this.storage.set(position, undefined);
+  }
+  retrieve(key) {
+    if (typeof key === 'number') {
+      return this.storage.get(key);
+    }
+    if (typeof key === 'string') {
+      key = key.toLowerCase();
+    }
+    const position = getIndexBelowMax(key, this.limit);
+    return this.storage.get(position);
+  }
 }
 
 module.exports = HashTable;
--- src/linked-list.js
+++ src/linked-list.js
@@ -4,7 +4,33 @@ class LinkedList {
     this.tail = null;
     // Do not modify anything inside of the constructor
   }
-  
+  addToTail(value) {
+    const newNode = {
+      value,
+      next: null
+    };
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
+  }
+  removeHead() {
+    const value = this.head.value;
+    this.head = this.head.next;
+    return value;
+  }
+  contains(value) {
+    let nodeWalker = this.head;
+    while (nodeWalker) {
+      if (nodeWalker.value === value) {
+        return true;
+      }
+      nodeWalker = nodeWalker.next;
+    }
+    return false;
+  }
 }
 
 module.exports = LinkedList;
