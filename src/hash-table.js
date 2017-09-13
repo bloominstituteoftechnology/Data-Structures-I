e5a513ae87098d47eabe05af7d26362716ff973e
Collin Ferguson <theredcouchroom@gmail.com>
2017-09-13 14:03:08 -0500

    Got core homework to pass, no extra credit. Don't fully understand the connection between the hash table and the limited array

--- src/hash-table.js
+++ src/hash-table.js
@@ -7,7 +7,23 @@ class HashTable {
     this.storage = new LimitedArray(this.limit);
     // Do not modify anything inside of the constructor
   }
-  
+  insert(key, value) {
+    this.storage[key] = value;
+    if (this.storage.length >= this.limit * 0.75) {
+      this.storage.push(new LimitedArray(this.limit * 2));
+    }
+  }
+  remove(key) {
+    if (key in this.storage) {
+      const previous = this.storage[key];
+      delete this.storage[key];
+      return previous;
+    }
+    return undefined;
+  }
+  retrieve(key) {
+    return (key in this.storage) ? this.storage[key] : undefined;
+  }
 }
 
 module.exports = HashTable;
--- src/linked-list.js
+++ src/linked-list.js
@@ -4,7 +4,38 @@ class LinkedList {
     this.tail = null;
     // Do not modify anything inside of the constructor
   }
-  
+  addToTail(value) {
+    const node = {};
+    node.value = value;
+    node.next = null;
+    if (this.head === null) {
+      this.head = node;
+    }
+    if (this.tail === null) {
+      this.tail = node;
+    }
+    this.tail.next = node;
+    this.tail = node;
+  }
+  removeHead() {
+    if (this.head === null) {
+      return null;
+    }
+    const oldHead = this.head.value;
+    this.head = this.head.next;
+    this.tail = this.tail.next;
+    return oldHead;
+  }
+  contains(value) {
+    let node = this.head;
+    while (node) {
+      if (node.value === value) {
+        return true;
+      }
+      node = node.next;
+    }
+    return false;
+  }
 }
 
 module.exports = LinkedList;
