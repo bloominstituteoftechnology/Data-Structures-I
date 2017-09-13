9abf35c0151a0c7d39e7c018894129579cac7a0b
sara fritz <krumsara@gmail.com>
2017-09-13 14:45:17 -0500

    homework finished, remove in hash table not working

--- src/linked-list.js
+++ src/linked-list.js
@@ -4,7 +4,49 @@ class LinkedList {
     this.tail = null;
     // Do not modify anything inside of the constructor
   }
-  
+  addToTail(value) {
+    const node = {
+      value,
+      next: null
+    };
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+  }
+
+  removeHead() {
+    if (this.head === null) {
+      return null;
+    }
+    if (this.head === this.tail) {
+      const value = this.head.value;
+      this.head = null;
+      this.tail = null;
+      return value;
+    }
+    const value = this.head.value;
+    this.head = this.head.next;
+    return value;
+  }
+  contains(value) {
+    if (this.head === null) {
+      return false;
+    }
+    const searchLinkedList = (node) => {
+      if (node.value === value) {
+        return true;
+      }
+      if (node.next === null) {
+        return false;
+      }
+      return searchLinkedList(node.next);
+    };
+    return searchLinkedList(this.head);
+  }
 }
 
 module.exports = LinkedList;
--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,16 @@
 class Queue {
-
+  constructor() {
+    this.arr = [];
+  }
+  enqueue(item) {
+    this.arr.push(item);
+  }
+  dequeue() {
+    return this.arr.shift();
+  }
+  get size() {
+    return this.arr.length;
+  }
 }
 
 module.exports = Queue;
