2facec1759f1b6286861e63e3e308e4707fcfcfa
ceylonsullen <hussainaljahmi@gmail.com>
2017-09-13 15:38:54 -0400

    passed main

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,16 @@
 class Stack {
-
+  constructor() {
+    this.storage = [];
+  }
+  pop() {
+    return this.storage.pop();
+  }
+  push(node) {
+    this.storage.push(node);
+  }
+  get size() {
+    return this.storage.length;
+  }
 }
 
 module.exports = Stack;
