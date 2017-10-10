66d6310f2bc064150711aaa6b9d35a7205d0c4d6
Kia Choi <sgchoi529@email.arizona.edu>
2017-10-10 15:44:07 -0700

    Kia's Commit - Hash-Table.js Progress

--- src/hash-table.js
+++ src/hash-table.js
@@ -13,19 +13,39 @@ class HashTable {
   // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
   // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
   insert(key, value) {
-
+    const index = getIndexBelowMax(key, this.limit);
+    const bucket = this.storage.get(index);
+    if (bucket === undefined) {
+      this.storage.set(index, value);
+    } else {
+      for (let i = 0; i < bucket.length; i++) {
+        if (bucket[i][0] === key) {
+          bucket[i][1] = value;
+        }
+      }
+    }
   }
   // Removes the key, value pair from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Remove the key, value pair from the bucket
   remove(key) {
-
+    const index = getIndexBelowMax(key, this.limit);
+    if (this.storage[index].length === 1 && this.storage.get(index) === key) {
+      delete this.storage[index];
+    } else {
+      for (let i = 0; i < this.storage[index].length; i++) {
+        if (this.storage.get(index) === key) {
+          delete this.storage[index];
+        }
+      }
+    }
   }
   // Fetches the value associated with the given key from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Find the key, value pair inside the bucket and return the value
   retrieve(key) {
-
+    const index = getIndexBelowMax(key, this.limit);
+    return this.storage.get(index);
   }
 }
 
