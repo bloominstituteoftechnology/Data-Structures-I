6552c1289870750323fecc6b1b8aa45694179056
Sam Kim <sams404link@gmail.com>
2017-10-10 15:44:29 -0700

    Data Structure updated

--- src/hash-table.js
+++ src/hash-table.js
@@ -13,13 +13,31 @@ class HashTable {
   // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
   // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
   insert(key, value) {
-
+    // definition of storage in constructor gives access to limited array class
+    this.storage.set(key, value);
+    const index = getIndexBelowMax(value, this.storage.length);
+    let bucket = this.storage[index];
+    if (!bucket) {
+      bucket = [];
+    }
+    bucket[key] = value;
+    let override = false;
+    // now iterate through our bucket to see if there are any conflicting
+    // key value pairs within our bucket. If there are any, override them.
+    for (let i = 0; i < bucket.length; i++) {
+      const tuple = bucket[i];
+      if (tuple[0] === key) {
+        // overide value stored at this key
+        tuple[1] = value;
+        override = true;
+      }
+    }
   }
   // Removes the key, value pair from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Remove the key, value pair from the bucket
   remove(key) {
-
+    delete this.storage[key];
   }
   // Fetches the value associated with the given key from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
