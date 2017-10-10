479a567fcff242a1e938c570fa580156365b7a3c
Sam Kim <sams404link@gmail.com>
2017-10-10 16:45:44 -0700

    DataStructures I updated

--- src/linked-list.js
+++ src/linked-list.js
@@ -16,15 +16,24 @@ class LinkedList {
     if (!this.head) {
       this.head = newObject;
       this.tail = newObject;
+    } else {
+      // This will set the previous node object's next property to the new object. Essentially, if there are two node
+      // objects, the first created one is now pointed to the second created node object.
+      this.tail.next = newObject;
+      // Tail will now be equal to the newly created node object.
+      this.tail = newObject;
     }
-    this.tail.next = newObject;
-    this.tail = newObject;
   }
   // Removes the current head node from the list, replacing it with the next element in the list
   // Returns the value of the removed node
   removeHead() {
+    // saves to variable the current value of head
     const value = this.head.value;
+    // move the head value to next property of the next object node in line in order the nodes objects were created.
+    // remember that next points to the next node object that was created. The ones created most recently is always
+    // the tail.
     this.head = this.head.next;
+    // return value of head that was removed
     return value;
   }
   // Checks the linked list for the given value
