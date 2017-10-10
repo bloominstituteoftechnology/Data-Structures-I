dbcfdff17396bb22d410335c04f49756e3653628
cmoore4os <cM2fzq3Faq3eKc6DCvjg6C>
2017-10-09 20:18:02 -0400

    passed stack and queue

--- src/queue.js
+++ src/queue.js
@@ -5,7 +5,23 @@
   4. Add a `dequeue` method that removes the item in the queue that was added earliest
 */
 class Queue {
-
+  constructor() {
+    this.queue = [];
+  }
+  get size() {
+    return this.queue.length;
+  }
+  // add an element to the end of queue
+  enqueue(element) {
+    this.queue.push(element);
+  }
+  // remove the first element from queue or return undefine if queue is empty
+  dequeue() {
+    if (this.size === 0) {
+      return;
+    }
+    return this.queue.shift();
+  }
 }
 
 module.exports = Queue;
