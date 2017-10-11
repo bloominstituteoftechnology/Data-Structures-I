ddf8c71996a553f8665bdace49415a755f6b3df1
Nicole Phillips <nphillips78@gmail.com>
2017-10-10 22:06:11 -0400

    linked-list complete

--- src/linked-list.js
+++ src/linked-list.js
@@ -25,12 +25,27 @@ class LinkedList {
   // Removes the current head node from the list, replacing it with the next element in the list
   // Returns the value of the removed node
   removeHead() {
-
+    const removed = this.head.value;
+    this.head = this.head.next;
+    return removed;
   }
   // Checks the linked list for the given value
   // Returns true if the the value is found in the list, false otherwise
-  contains(value) {
+  // while loop
+  // what is the condition?
+  // while node is current, if node contains value, return true
+  // iterate through each node
+  // return false when value isn't found
 
+  contains(value) {
+    let current = this.head;
+    while (current.next !== null) {
+      if (current.value === value) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
   }
 }
 
