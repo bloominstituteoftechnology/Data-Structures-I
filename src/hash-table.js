820e537a80eb2fbb6152d46371922532bcf5d1ce
Christian Franco <christian.franco94@icloud.com>
2017-10-11 11:26:16 -0700

    finished extra credit

--- src/hash-table.js
+++ src/hash-table.js
@@ -32,7 +32,16 @@ class HashTable {
     const index = getIndexBelowMax(key, this.limit);
     const bucket = new Bucket(this.storage.get(index));
     bucket.add({ key, value });
-    this.storage.set(index, bucket.dump());
+
+    if (this.storage.length >= this.limit * 0.75) {
+      const prevStorage = this.storage;
+      this.limit *= 2;
+      this.storage = new LimitedArray((this.limit));
+      prevStorage.each((item, i) => {
+        this.storage.set(i, item);
+      });
+    }
+    this.storage.set(index, [...bucket.dump()]);
   }
   // Removes the key, value pair from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
@@ -42,7 +51,7 @@ class HashTable {
     const bucket = new Bucket(this.storage.get(index));
     this.storage.set(index, bucket.dump().filter(item => item.key !== key));
   }
-  // Fetches the value associated with the given key from the hash table
+  // Fetches the value associated with the giv en key from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Find the key, value pair inside the bucket and return the value
   retrieve(key) {
@@ -51,6 +60,29 @@ class HashTable {
     return bucket.findValue(key);
   }
 }
+
+// const k = new HashTable();
+// console.log('Inserted', 1);
+// k.insert(0, 'First Value');
+// console.log('Inserted', 2);
+// k.insert(1, 'Second Value');
+// console.log('Inserted', 3);
+// k.insert('a', true);
+// console.log('Inserted', 4);
+// k.insert('a', false);
+// console.log('Inserted', 5);
+// k.insert('b', true);
+// console.log('Inserted', 6);
+// console.log('should increase');
+// k.insert('c', true);
+// console.log('Inserted', 7);
+// k.insert('d', true);
+// console.log('Inserted', 8);
+// k.insert('e', true);
+// console.log('Inserted', 9);
+// k.insert('f', true);
+// console.log('Inserted', 10);
+// k.insert('g', true);
 
 // class HashTable {
 //   constructor(limit = 8) {
