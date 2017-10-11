17fc57f4e4e6c2b4b2ce2fc3c61b874896fda046
Jeff <admin@etisdew.website>
2017-10-11 02:13:53 -0700

    Update linked-list.js

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,25 +1,47 @@
 /* eslint-disable class-methods-use-this */
+
 class LinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
-    // Do not modify anything inside of the constructor
   }
-  // Wraps the given value in a node object and adds the node to the tail of the list
-  // If the list is empty, the new element is considered the tail as well as the head
-  // If there is one element in the list before the new element is added, the new element becomes the tail of the list
+  size() {
+    let current = this.head;
+    for (let count = 0; current !== null; ++count) {
+      current = current.next;
+    }; return count;
+  }
+
+  addToHead(value) {
+    const newNode = {
+      value,
+      previous: null,
+      next: this.head
+    }; this.head = newNode;
+  }
+
   addToTail(value) {
-
+    const node = { value, previous: null, next: null}
+    if (this.head === null) { this.head, node.previous, this.tail = node, node, node; return}
+    let current = this.tail;
+    while (current.next !== null) {
+      current = this.next;
+    }; this.tail.next, node.previous, this.tail = node, this.tail, node;
   }
-  // Removes the current head node from the list, replacing it with the next element in the list
-  // Returns the value of the removed node
+
   removeHead() {
-
+    if (this.head === null) return;
+    let _return = this.head.value;
+    this.head = this.head.next;
+    return _return;
   }
-  // Checks the linked list for the given value
-  // Returns true if the the value is found in the list, false otherwise
-  contains(value) {
 
+  contains(value) {
+    let current = this.head;
+    while (current !== null) {
+      if (current.value === value) return true;
+      current = current.next;
+    }; return false;
   }
 }
 
