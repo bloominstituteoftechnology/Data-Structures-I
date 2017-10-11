fae4b891192de5948117051f20a211d6903bc4ae
Perry Ahern <pahern@gmail.com>
2017-10-11 13:01:53 -0400

    Perry Ahern - all 4 completed, no extra credit

--- src/hash-table.js
+++ src/hash-table.js
@@ -13,44 +13,61 @@ class HashTable {
   // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
   // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
   insert(key, value) {
-    if (typeof key === 'number') key = key.toString();
+    // const strKey = typeof key === 'number' ? key.toString() : key;
     const index = getIndexBelowMax(key, this.limit);
-    if (this.storage.get(index) === undefined) {          // there is no bucket; create it and store in new array
-      this.storage.set(index, [key, value]);
-      console.log(this.storage.get(index));
-    } else if (Array.isArray(this.storage.get(index))) {   // bucket with key exists, replace key's value
-      console.log('else if');
-      // this.storage.each(this.storage.set(index, value));
-      // console.log(this.storage.storage);
-    } else {                                          // bucket(s) exist, add another to array
-      console.log('else');
-      // this.storage.set(index, value);
-      // console.log(this.storage.storage);
+    const bucket = this.storage;
+
+    if (bucket.get(index) === undefined) {      // No bucket at index yet
+      bucket.set(index, [[key, value]]);        // Create bucket containing a k/v pair
+      return;                                   // Completely done. Exit the insert function.
     }
-    // if (this.storage.storage[index] === undefined) {          // there is no bucket; create it and store in new array
-    //   // this.storage.storage[index] = [[key, value]];
-    //   this.set(index, value);
-    // } else if (this.storage.storage[index][0][0] === key) {   // bucket with key exists, replace key's value
-    //   // this.storage.storage[index][0][1] = value;
-    //   this.set(index[0][1], value);
-    // } else {                                          // bucket(s) exist, add another to array
-    //   // this.storage.storage[index].push([key, value]);
-    //   this.set(index, value);
-    // }
-    // console.log(JSON.stringify(this));
-    // return this.storage.storage;
+
+    let keyFound = false;                     // flag to hold state: key present in bucket?
+    bucket.get(index).forEach((kvPair) => {
+      if (kvPair[0] === key) {                // Check to see if bucket already has key
+        kvPair[1] = value;                    // Found; set kvPair value to new value
+        keyFound = true;                      // Set flag true so kvPair won't be added again at end
+        return;
+      }
+    });
+    if (!keyFound) bucket.get(index).push([key, value]);  // If key wasn't found in bucket, add k/v pair
   }
+
   // Removes the key, value pair from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Remove the key, value pair from the bucket
   remove(key) {
+    // const strKey = typeof key === 'number' ? key.toString() : key;
+    const index = getIndexBelowMax(key, this.limit);
+    const bucket = this.storage;
 
+    if (bucket.get(index) !== undefined) {         // Verify there's a bucket at index
+      bucket.get(index).forEach((kvPair, i) => {   // Cycle through k/v pairs, with i as forEach index
+        if (kvPair[0] === key) {                   // If pair has the desired key
+          bucket.get(index).splice(i, 1);          // Cut out one pair at that i (forEach index)
+          return;                                  // Exit the anonymous function
+        }
+      });
+    }
   }
   // Fetches the value associated with the given key from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Find the key, value pair inside the bucket and return the value
   retrieve(key) {
+    // const strKey = typeof key === 'number' ? key.toString() : key;
+    const index = getIndexBelowMax(key, this.limit);
+    const bucket = this.storage;
+    let value;
 
+    if (bucket.get(index) !== undefined) {         // Verify there's a bucket at index
+      bucket.get(index).forEach((kvPair) => {      // Cycle through k/v pairs
+        if (kvPair[0] === key) {                   // If pair has the desired key
+          value = kvPair[1];                       // Save the key's associated value
+          return;                                  // Exit the anonymous function
+        }
+      });
+      return value;                                // Return the saved value
+    }
   }
 }
 
