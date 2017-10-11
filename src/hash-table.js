14a3813236567d92521de9772efb91ce3f24b95d
Christian Franco <christian.franco94@icloud.com>
2017-10-11 03:34:13 -0700

    Initial Hashtable solution

--- src/hash-table.js
+++ src/hash-table.js
@@ -13,19 +13,44 @@ class HashTable {
   // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
   // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
   insert(key, value) {
+    const index = getIndexBelowMax(key, this.limit);
+    let bucket = this.storage.get(index);
 
+    if (bucket === undefined) {
+      // create bucket
+      this.storage.set(index, []);
+    }
+
+    bucket = [...this.storage.get(index).filter(item => item.key !== key), { key, value }];
+    this.storage.set(index, bucket);
   }
   // Removes the key, value pair from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Remove the key, value pair from the bucket
   remove(key) {
-
+    const index = getIndexBelowMax(key, this.limit);
+    const bucket = this.storage.get(index);
+    if (bucket === undefined) {
+      return;
+    }
+    this.storage.set(index, bucket.filter(item => item.key !== key));
   }
   // Fetches the value associated with the given key from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Find the key, value pair inside the bucket and return the value
   retrieve(key) {
-
+    const index = getIndexBelowMax(key, this.limit);
+    const bucket = this.storage.get(index);
+    if (bucket === undefined) {
+      return undefined;
+    }
+    const results = bucket.filter((item) => {
+      return item.key === key;
+    });
+    if (results.length === 0) {
+      return undefined;
+    }
+    return results[0].value;
   }
 }
 
