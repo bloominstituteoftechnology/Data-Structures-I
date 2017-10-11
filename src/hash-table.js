84a5c768641822ef37f87d7668b541baad6c5142
Aaron Artea <aarongitis@gmail.com>
2017-10-11 12:42:35 -0700

    Did not see helper class, oops!

--- src/hash-table.js
+++ src/hash-table.js
@@ -13,12 +13,43 @@ class HashTable {
   // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
   // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
   insert(key, value) {
+    //  Locate our bucket and index it via use of getIndexBelowMax function.
+    const index = getIndexBelowMax(key, this.limit);
 
+    if (this.storage[index] === undefined) {
+      //  In our object, store key-value pair
+      this.storage[index] = [[key, value]];
+    } else {
+      //  Need to scan through object to place value in our hash table. Our MAX value is found with the length of storage[index]. Recall: key is at pos 0, val is pos 1.
+      //  Use flag to indicate value overwrites, else, it's an unused index!
+      let flag = false;
+      for (let i = 0; i < this.storage[index].length; i++) {
+        if (this.storage[index][i][0] === key) {
+          // Update old value; overwrite.
+          this.storage[index][i][1] = value;
+          flag = true;
+        }
+      }
+      if (flag === false) {
+        this.storage[index].push([key, value]);
+      }
+    }
   }
   // Removes the key, value pair from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Remove the key, value pair from the bucket
   remove(key) {
+    const index = getIndexBelowMax(key, this.limit);
+
+    if (this.storage[index] === undefined) {
+      return undefined;
+    }
+
+    for (let i = 0; i < this.storage[index].length; i++) {
+      if (this.storage[index][i][0] === key) {
+        return this.storage[index][i][1];
+      }
+    }
 
   }
   // Fetches the value associated with the given key from the hash table
