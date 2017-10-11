3b294d633e41c875bf8eeb2e46406c9ec2300bc9
David Long <davdaarn@live.com>
2017-10-11 08:34:16 -0500

    reworked hash table

--- src/hash-table.js
+++ src/hash-table.js
@@ -8,25 +8,14 @@ class HashTable {
     this.storage = new LimitedArray(this.limit);
     // Do not modify anything inside of the constructor
   }
-  // Adds the given key, value pair to the hash table
-  // Fetch the bucket associated with the given key using the getIndexBelowMax function
-  // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
-  // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
   insert(key, value) {
     if (typeof key === 'number') {
-      // this.storage.storage[key] = value;
-      this.storage.set(key, value);
-      // console.log('number was a key');
+      this.storage.set(key, [key] = value);
     } else {
-      // this.storage.storage[getIndexBelowMax(key, this.limit)] = value;
-      this.storage.set(getIndexBelowMax(key, this.limit), value);
-      // console.log('key was string');
+      this.storage.set(getIndexBelowMax(key, this.limit), [key] = value);
+      // this.storage.set(getIndexBelowMax(key, this.limit), value);
     }
-    // console.log(typeof key);
   }
-  // Removes the key, value pair from the hash table
-  // Fetch the bucket associated with the given key using the getIndexBelowMax function
-  // Remove the key, value pair from the bucket
   remove(key) {
     if (typeof key === 'number') {
       this.storage.remove(key);
@@ -37,9 +26,6 @@ class HashTable {
       }
     }
   }
-  // Fetches the value associated with the given key from the hash table
-  // Fetch the bucket associated with the given key using the getIndexBelowMax function
-  // Find the key, value pair inside the bucket and return the value
   retrieve(key) {
     if (typeof key === 'number') {
       // return this.storage.storage[key];
