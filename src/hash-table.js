c8a6edf7e961c06788ee79294609aa3fa724635f
Perry Ahern <pahern@gmail.com>
2017-10-10 20:20:12 -0400

    refactoring to use the helper methods

--- src/hash-table.js
+++ src/hash-table.js
@@ -13,7 +13,32 @@ class HashTable {
   // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
   // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
   insert(key, value) {
-
+    if (typeof key === 'number') key = key.toString();
+    const index = getIndexBelowMax(key, this.limit);
+    if (this.storage.get(index) === undefined) {          // there is no bucket; create it and store in new array
+      this.storage.set(index, [key, value]);
+      console.log(this.storage.get(index));
+    } else if (Array.isArray(this.storage.get(index))) {   // bucket with key exists, replace key's value
+      console.log('else if');
+      // this.storage.each(this.storage.set(index, value));
+      // console.log(this.storage.storage);
+    } else {                                          // bucket(s) exist, add another to array
+      console.log('else');
+      // this.storage.set(index, value);
+      // console.log(this.storage.storage);
+    }
+    // if (this.storage.storage[index] === undefined) {          // there is no bucket; create it and store in new array
+    //   // this.storage.storage[index] = [[key, value]];
+    //   this.set(index, value);
+    // } else if (this.storage.storage[index][0][0] === key) {   // bucket with key exists, replace key's value
+    //   // this.storage.storage[index][0][1] = value;
+    //   this.set(index[0][1], value);
+    // } else {                                          // bucket(s) exist, add another to array
+    //   // this.storage.storage[index].push([key, value]);
+    //   this.set(index, value);
+    // }
+    // console.log(JSON.stringify(this));
+    // return this.storage.storage;
   }
   // Removes the key, value pair from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
