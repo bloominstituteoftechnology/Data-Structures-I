42c5cfa0b7dc283c056577df0326aef8463fab45
Jeff <admin@etisdew.website>
2017-10-11 01:14:03 -0700

    Update stack.js

--- src/stack.js
+++ src/stack.js
@@ -4,8 +4,26 @@
   3. Add a `push` method that accepts an item as input and adds it to the storage structure
   4. Add a `pop` method that removes the most recently-added item to the stack
 */
-class Stack {
 
+class Stack {
+	constructor(){ this.closure = []}
+	Size(){ return (this.closure.length ? this.closure.length : 0)}
+	Push(tuple){ this.closure.push(tuple)}
+	Pop(){ if (this.closure.length > 0) this.closure.pop()}
 }
 
 module.exports = Stack;
+
+
+const logit = function(stack){ console.log(
+	'this: ', stack,
+	'\nthis.Size():', stack.Size(), '...end of test')}
+
+const test = new Stack();
+test.Push(['key1', 'value1']); logit(test);
+test.Push(['key2', 'value2']); logit(test);
+test.Pop(); logit(test);// 2
+test.Push(['key3', 'value3']); logit(test); // 3
+test.Pop(); logit(test); // 2
+test.Pop(); logit(test); // 0
+test.Pop(); logit(test); // testing conditional
