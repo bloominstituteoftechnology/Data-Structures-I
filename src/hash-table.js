7b62d933c4379a1b6b602a84c00452091e623aba
Jaosn Nuhn <jasonnuhn@Jaosns-Air.fios-router.home>
2017-10-11 15:00:59 -0400

    final commit Jason Nuhn

--- src/hash-table.js
+++ src/hash-table.js
@@ -13,13 +13,24 @@ class HashTable {
   // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
   // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
   insert(key, value) {
-
+    const index = this.hashFunc(key, this.limit);
+    const bucket = this.storage[index];
+    getIndexBelowMax = function() {
+      if (bucket = null) {
+      let bucket = [];
+        this.storage[index] = bucket;
+      }
+    };
   }
   // Removes the key, value pair from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Remove the key, value pair from the bucket
   remove(key) {
-
+    const index = this.hashFunc(key, this.limit);
+    const bucket = this.storage[index];
+    if (!bucket) {
+      return null;
+    }
   }
   // Fetches the value associated with the given key from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
