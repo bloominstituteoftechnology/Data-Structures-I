0c592284d4404f2fe1234c5224369614e91d0091
Sneha Thadani <snehacopper@gmail.com>
2017-10-11 23:25:21 -0700

    hashtable with errors

--- src/hash-table.js
+++ src/hash-table.js
@@ -16,13 +16,15 @@ class HashTable {
   // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
   // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
   insert(key, value) {
-    const bucketIndex = this.storage.getIndexBelowMax(key, this.limit);
+    const bucketIndex = getIndexBelowMax(key, this.limit);
     const bucket = this.storage.get(bucketIndex);
     if (bucket === null) {
       const newBucket = {
         key,
         value,
       };
+      this.storage.set(bucketIndex, newBucket);
+      console.log(this.storage);
     } else {
       bucket.value = value;
     }
@@ -32,7 +34,7 @@ class HashTable {
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Remove the key, value pair from the bucket
   remove(key) {
-    const bucketIndex = this.storage.getIndexBelowMax(key, this.limit);
+    const bucketIndex = getIndexBelowMax(key, this.limit);
     if (this.storage.get(bucketIndex) === null) {
       return;
     }
@@ -42,7 +44,7 @@ class HashTable {
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Find the key, value pair inside the bucket and return the value
   retrieve(key) {
-    const bucketIndex = this.storage.getIndexBelowMax(key, this.limit);
+    const bucketIndex = getIndexBelowMax(key, this.limit);
     const bucket = this.storage.get(bucketIndex);
     if (bucket === null) {
       return null;
