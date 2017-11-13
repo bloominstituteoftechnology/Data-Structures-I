ea3e0769206b4b84fc10545dd05dbad01a7ca64c
Vinnie <vinnie.scalco@gmail.com>
2017-11-13 16:55:23 -0600

    Linked List, Queue, Stack Complete

--- src/stack.js
+++ src/stack.js
@@ -5,6 +5,38 @@
   4. Add a `pop` method that removes the most recently-added item to the stack
 */
 class Stack {
+  constructor() {
+    this.storage = {};
+    this.size = 0;
+  }
+
+  push(value) {
+    this.storage[this.size] = value;
+    this.size++;
+  }
+
+
+  pop() {
+    if (this.size === 0) {
+      return undefined;
+    }
+
+    this.size--;
+    const result = this.storage[this.size];
+    delete this.storage[this.size];
+    return result;
+  }
+
+
+  size() {
+    return this.size;
+  }
+
+
+  amtStore(value) {
+    if (value === undefined) return this.storage[this.size - 1];
+    return this.storage[value];
+  }
 
 }
 
