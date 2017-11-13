193be47f66d46b89d3807de6308f0a6bb0eecff2
Emma Drueke <emma.drueke@gmaila.com>
2017-11-13 17:02:25 -0600

    Linked List completed

--- src/linked-list.js
+++ src/linked-list.js
@@ -5,22 +5,53 @@ class LinkedList {
     this.tail = null;
     // Do not modify anything inside of the constructor
   }
+
   // Wraps the given value in a node object and adds the node to the tail of the list
   // If the list is empty, the new element is considered the tail as well as the head
   // If there is one element in the list before the new element is added, the new element becomes the tail of the list
   addToTail(value) {
-
+    const newTail = {
+      value,
+      next: null,
+    };
+    if (!this.head) {
+      this.head = newTail;
+      this.tail = newTail;
+    } else {
+      this.tail.next = newTail;
+      this.tail = newTail;
+    }
   }
   // Removes the current head node from the list, replacing it with the next element in the list
   // Returns the value of the removed node
   removeHead() {
+    if (!this.head) return null;
+    // if we remove the last head, also set the tail to null, otherwise retains the last value;
+    if (!this.head.next) this.tail = null;
 
+    const node = this.head.value;
+    this.head = this.head.next;
+    return node;
   }
   // Checks the linked list for the given value
   // Returns true if the the value is found in the list, false otherwise
   contains(value) {
-
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      if (currentNode.value === value) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
   }
 }
 
 module.exports = LinkedList;
+
+// const l = new LinkedList;
+// l.addToTail('Dog');
+// l.addToTail('Frog');
+// l.addToTail('Bog');
+// console.log(l);
+// console.log(l.removeHead());
