3f43a6af1c41bdc476076d7eb9f0982bbed22853
Aaron Wilder <element1028@gmail.com>
2017-11-13 14:10:55 -0800

    finished stack.js

--- src/stack.js
+++ src/stack.js
@@ -5,7 +5,26 @@
   4. Add a `pop` method that removes the most recently-added item to the stack
 */
 class Stack {
-
+  constructor() {
+    this.storage = {};
+    this.count = 0;
+  }
+  get size() {
+    return this.count;
+  }
+  push(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+  pop() {
+    if (this.count === 0) {
+      return undefined;
+    }
+    this.count--;
+    const result = this.storage[this.count];
+    delete this.storage[this.count];
+    return result;
+  }
 }
 
 module.exports = Stack;
