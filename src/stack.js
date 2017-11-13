72ea395eef39917bda9eb9d5df52e2d4a0118a50
Franz <franzjfernando@hotmail.com>
2017-11-13 13:45:30 -0800

    added code to stack.js

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,21 @@
 class Stack {
-
+  constructor() {
+    this.stack = [];
+    this.size = 0;
+  }
+  pop() {
+    if (this.size > 0) this.count -= 1;
+    return this.stack.pop();
+  }
+  push(value) {
+    this.stack.push(value);
+    this.size += 1;
+  }
+  getSize() {
+    return this.size;
+  }
 }
+const myStack = new Stack();
+console.log(myStack.push('name'));
 
 module.exports = Stack;
