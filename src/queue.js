51abb7440d59ebba62660850a46573d27918fc21
dreamingrainbow <michaeladennis@yahoo.com>
2017-11-13 16:44:07 -0700

    passign all test for queues

--- src/queue.js
+++ src/queue.js
@@ -5,6 +5,36 @@
   4. Add a `dequeue` method that removes the item in the queue that was added earliest
 */
 class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.i = 0;
+  }
+  enqueue(v) {
+    this.i++;
+    const newNode = {
+      v,
+      next: null,
+    };
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+  dequeue() {
+    this.i = (this.i > 1) ? this.i - 1 : 0;
+    if (!this.head) return 0;
+    if (!this.head.next) this.tail = null;
+    const returnValue = this.head.v;
+    this.head = this.head.next;
+    return returnValue;
+  }
+  get size() {
+    return this.i;
+  }
 
 }
 
