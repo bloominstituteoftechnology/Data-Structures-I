5a0e721bf07ab68385dff1714dfc5fc9a21abf28
Aaron Wilder <element1028@gmail.com>
2017-11-14 15:05:42 -0800

    just need one more test to pass!!!!

--- src/hash-table.js
+++ src/hash-table.js
@@ -16,24 +16,44 @@ class HashTable {
   // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
   // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
   insert(key, value) {
-    const index = getIndexBelowMax(key, this.limit);
-    // console.log(index);
-    if (this.storage[index] === undefined) {
-      this.storage.set(index, value);
-      this.storage.get(index);
-    } else {
-      let inserted = false;
-      for (let i = 0; i < this.storage[index].length; i++) {
-        if (this.storage[index][i][0] === key) {
-          this.storage[index][i][1] = value;
-          inserted = true;
-        }
+    if (typeof key === 'number') {
+      if (this.storage.get(key) === undefined) {
+        const values = [];
+        values[key] = value;
+        return this.storage.set(key, values);
       }
-      if (inserted === false) {
-        this.storage[index].push([key, value]);
+      const bucket = this.storage.get(key);
+      let x;
+      bucket.forEach((ele, i) => {
+        if (ele.indexOf(key) !== -1) {
+          x = i;
+        }
+      });
+      if (x === undefined) {
+        bucket.push([key, value]);
+      } else {
+        bucket[x] = [key, value];
       }
     }
-    // console.log(this.storage.get(index));
+    const index = getIndexBelowMax(key, this.limit);
+    if (this.storage.get(index) === undefined) {
+      this.storage.set(index, [[key, value]]);
+    } else {
+      const bucket = this.storage.get(index);
+      let x;
+      bucket.forEach((ele, i) => {
+        if (ele.indexOf(key) !== -1) {
+          x = i;
+        }
+      });
+      if (x === undefined) {
+        bucket.push([key, value]);
+      } else {
+        bucket[x] = [key, value];
+      }
+      //  if (f)
+      // console.log(bucket);
+    }
     return this.storage.get(index);
   }
   // Removes the key, value pair from the hash table
@@ -41,18 +61,49 @@ class HashTable {
   // Remove the key, value pair from the bucket
 
   remove(key) {
-
+    if (typeof key === 'number') {
+      const bucket = this.storage.get(key);
+      bucket.forEach((ele, i) => {
+        if (ele[0] === key) {
+          delete bucket[i];
+        }
+      });
+    }
+    const index = getIndexBelowMax(key, this.limit);
+    const bucket = this.storage.get(index);
+    if (bucket === undefined) return;
+    bucket.forEach((ele, i) => {
+      if (ele[0] === key) {
+        delete bucket[i];
+      }
+    });
   }
   // Fetches the value associated with the given key from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Find the key, value pair inside the bucket and return the value
   retrieve(key) {
-
+    if (typeof key === 'number') {
+      const bucket = this.storage.get(key);
+      bucket.forEach((ele) => {
+        if (ele[0] === key) return ele[1];
+      });
+    }
+    const nKey = getIndexBelowMax(key, this.limit);
+    const bucket = this.storage.get(nKey);
+    if (bucket === undefined) return;
+    let i;
+    bucket.forEach((ele) => {
+      if (key === ele[0]) {
+        i = ele[1];
+      }
+    });
+    return i;
   }
 }
-// const newHash = new HashTable();
-// console.log(newHash);
-// newHash.insert('aaron',34);
-// console.log(getIndexBelowMax('aaron', 3));
-// console.log(newHash)
+const hashTable = new HashTable();
+hashTable.insert(0, 'First Value');
+hashTable.insert(1, 'Second Value');
+console.log(hashTable.retrieve(0), 'First Value');
+console.log(hashTable.retrieve(1), 'Second Value');
+
 module.exports = HashTable;
