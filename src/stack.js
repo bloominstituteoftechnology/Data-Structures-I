c530c22a486af426a9a97576932ed0acedf5b901
Ting Wang <felix.wang.1026@gmail.com>
2017-11-13 19:20:26 -0500

    Completed the stack requirements by using array

--- src/stack.js
+++ src/stack.js
@@ -5,7 +5,32 @@
   4. Add a `pop` method that removes the most recently-added item to the stack
 */
 class Stack {
+  constructor() {
+    this.length = 0;
+    this.top = null;
+    this.stackArray = [];
+  }
 
+  push(value) {
+    this.stackArray.push(value);
+    this.top = this.stackArray[this.stackArray.length - 1];
+    this.length++;
+  }
+
+  pop() {
+    const popValue = this.stackArray.pop();
+    this.top = this.stackArray[this.stackArray.length - 1];
+    if (this.stackArray.length === 0) {
+      this.length = 0;
+    } else {
+      this.length--;
+    }
+    return popValue;
+  }
+
+  get size() {
+    return this.length;
+  }
 }
 
 module.exports = Stack;
