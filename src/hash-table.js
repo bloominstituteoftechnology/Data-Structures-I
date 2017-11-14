8c2ef24a193d501d48e48b7c66150bba87271c50
FJF616 <franzjfernando@hotmail.com>
2017-11-13 19:29:34 -0800

    fixing errors on hashtable

--- src/hash-table.js
+++ src/hash-table.js
@@ -5,32 +5,50 @@ class HashTable {
   constructor() {
     this.limit = 8;
     this.storage = new LimitedArray(this.limit);
+
     // Do not modify anything inside of the constructor
   }
+  hashNode(key, value, next) {
+    this.key = key;
+    this.value = value;
+    this.next = next || null;
+    }
+  hashfunc(key) {
+    let total = 0;
+    this.buckets = this.storage.length;
+    for (let i = 0; i < key.length; i++) {
+       total += key.charCodeAt(i);
+    }
+    let bucket = total % this.buckets;
+    return bucket;
+    }
   insert(key, value) {
-    let pos = this.hashFunc(key, this.limit);
-    let bucket = this.storage[pos]
-    if (!bucket) {
-      let bucket = [];
-      this.storage[pos] = bucket;
-    }
-    let fixConflict = false;
-    for (let i = 0; i < bucket.length; i++) {
-      let tuple = bucket[i];
-      if (tuple[0] === key) {
-        tuple[1] = value;
-        fixConflict = true;
+    let index = this.hashfunc(key);
+    if (!this.storage[index]) {
+      this.storage[index] = new hashNode(key, value);
+    } else if (this.storage[index].key === key) {
+        this.storage[index].value = value;
+    } else {
+      let currentNode = this.storage[index];
+       while (currentNode.next) {
+        if (currentNode.next.key === key) {
+          currentNode.next.value = value;
+          return
+        }
+        currentNode = currentNode.next;
       }
+      currentNode.next = new hashNode(key, value);
+    }  
+  }
+  retrieve(key) {
+    let index = this.hashfunc(key); 
+    if (!this.storage[index]) return null;
+    let currentNode = this.storage[index];
+    while (currentNode) {
+      if (currentNode.key === key) return currentNode.value
+      currentNode = currentNode.next;
+      }
+      return null;
     }
-  };
-  hashFunc(str, max) {
-    let hash = 0;
-    for (let i = 0;i < str.length; i++) {
-      let letter = str[i];
-      hash = (hash << 5) + letter.charCodeAt(0);
-      hash = (hash & hash) % max;
-    }
-    return hash;
-  };
 }
 module.exports = HashTable;
