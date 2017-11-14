ab95a85b0feeff1d391ccc00074c060803377eaf
FJF616 <franzjfernando@hotmail.com>
2017-11-14 15:06:29 -0800

    still attempting hastables

--- src/hash-table.js
+++ src/hash-table.js
@@ -5,50 +5,45 @@ class HashTable {
   constructor() {
     this.limit = 8;
     this.storage = new LimitedArray(this.limit);
-
     // Do not modify anything inside of the constructor
   }
-  hashNode(key, value, next) {
-    this.key = key;
-    this.value = value;
-    this.next = next || null;
+
+  hashtable() {
+    const max = this.bucketCount;
+    this.bucketCount = this.limit;
+    this.buckets = this.storage;
+    for (let i = 0; i < this.bucketCount; i++) {
+      this.buckets.push(new HashTable());
     }
-  hashfunc(key) {
-    let total = 0;
-    this.buckets = this.storage.length;
-    for (let i = 0; i < key.length; i++) {
-       total += key.charCodeAt(i);
-    }
-    let bucket = total % this.buckets;
-    return bucket;
-    }
+  }
+  hashFunction(str) {
+    const max = this.bucketCount;
+    return this.storage.getIndexBelow(str, max);
+  }
+
   insert(key, value) {
-    let index = this.hashfunc(key);
-    if (!this.storage[index]) {
-      this.storage[index] = new hashNode(key, value);
-    } else if (this.storage[index].key === key) {
-        this.storage[index].value = value;
-    } else {
-      let currentNode = this.storage[index];
-       while (currentNode.next) {
-        if (currentNode.next.key === key) {
-          currentNode.next.value = value;
-          return
-        }
-        currentNode = currentNode.next;
-      }
-      currentNode.next = new hashNode(key, value);
-    }  
+    const index = this.hashFunction(key);
+    if (!this.buckets.checkLimit(index)) {
+      this.buckets.set(index, value);
+      this.bucketCount++;
+    }
+  }
+
+  remove(key) {
+    const index = this.buckets.retrieve(key);
+    const hash = this.buckets.get(index);
+    if (this.buckets.values.hasProperty(hash) && this.buckets.values[hash].hasProperty(index)) {
+      delete this.storage.values[hash][index];
+      this.bucketCount--;
+    }
   }
   retrieve(key) {
-    let index = this.hashfunc(key); 
-    if (!this.storage[index]) return null;
-    let currentNode = this.storage[index];
-    while (currentNode) {
-      if (currentNode.key === key) return currentNode.value
-      currentNode = currentNode.next;
-      }
-      return null;
+    const index = this.hashFunction(key);
+    const hash = this.buckets.get(index);
+    if (this.buckets.values.hasProperty(hash) && this.buckets.values[hash].hasProperty(index)) {
+      return this.buckets.values[hash][index];
     }
+    return null;
+  }
 }
 module.exports = HashTable;
