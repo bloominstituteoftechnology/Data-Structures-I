937ff348ff83dddc72eb72fabce4779381e72e89
Stephanie <sbowl001@fiu.edu>
2017-11-15 06:21:27 +0800

    newest comments

--- src/linked-list.js
+++ src/linked-list.js
@@ -3,6 +3,7 @@ class LinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
+    this.size = 0; // not need it for this problem, but left in case for future notes
     // Do not modify anything inside of the constructor
   }
   // Wraps the given value in a node object and adds the node to the tail of the list
@@ -13,31 +14,37 @@ class LinkedList {
       value,
       next: null,
     };
+    // not set yet
     if (!this.head) {
       this.head = newNode;
       this.tail = newNode;
     } else {
-      this.tail.next = newNode;
+      this.tail.next = newNode; // change tail value and add tail.next
       this.tail = newNode;
     }
+    this.size++;
   }
   // Removes the current head node from the list, replacing it with the next element in the list
   // Returns the value of the removed node
   removeHead() {
     const removedHead = this.head.value;
     this.head = this.head.next;
+    this.size--;
     return removedHead;
   }
   // Checks the linked list for the given value
   // Returns true if the the value is found in the list, false otherwise
   contains(value) {
-    let found = false;
+    let found = false; // guilty until proven true
     let checkedNode = this.head;
     while (checkedNode) {
       if (checkedNode.value === value) {
         found = true;
       }
-      checkedNode = checkedNode.next; // don't understand this part. Allows to continue checking the list?
+      checkedNode = checkedNode.next;
+      // need checkedNode because we don't wanna change this.head
+      // the next next becomes the head
+      // Allows to continue checking the list
     } return found;
   }
 }
