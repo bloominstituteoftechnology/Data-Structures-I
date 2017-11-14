1b01e951de08986a976836b6ec272089ac627364
Ismail Jadun <1683380+pinwale@users.noreply.github.com>
2017-11-13 19:36:58 -0500

    fixed indents

--- src/stack.js
+++ src/stack.js
@@ -4,26 +4,26 @@
   3. Add a `push` method that accepts an item as input and adds it to the storage structure
   4. Add a `pop` method that removes the most recently-added item to the stack
 */
-  class Stack {
-    constructor() {
+class Stack {
+  constructor() {
       this.storage = [];
-      return this.counter = 0;
-    }
-    push(value) {
-      this.counter++;
-      this.storage.push(value);
-    }
-    pop() {
-      this.counter--;
-      if (this.counter < 0) {
-        this.counter = 0;
-        return this.counter;
-      }
-      return this.storage.pop();
-    }
-    get size() {
+    return this.counter = 0;
+  }
+  push(value) {
+    this.counter++;
+    this.storage.push(value);
+  }
+  pop() {
+    this.counter--;
+    if (this.counter < 0) {
+      this.counter = 0;
       return this.counter;
     }
+      return this.storage.pop();
+    }
+  get size() {
+    return this.counter;
   }
+}
 
   module.exports = Stack;
