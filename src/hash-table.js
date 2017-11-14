7a352d8b5b00535b432c942aa28ed1eac889428d
FJF616 <franzjfernando@hotmail.com>
2017-11-13 16:33:45 -0800

    added code to has-table

--- src/hash-table.js
+++ src/hash-table.js
@@ -8,14 +8,29 @@ class HashTable {
     // Do not modify anything inside of the constructor
   }
   insert(key, value) {
-    let previous;
-    if (typeof this.storage[key] !== 'string') {
-      previous = this.storage[key];
-    } else {
-      this.length++;
+    let pos = this.hashFunc(key, this.limit);
+    let bucket = this.storage[pos]
+    if (!bucket) {
+      let bucket = [];
+      this.storage[pos] = bucket;
     }
-    this.storage[key] = value;
-    return previous;
-  }
+    let fixConflict = false;
+    for (let i = 0; i < bucket.length; i++) {
+      let tuple = bucket[i];
+      if (tuple[0] === key) {
+        tuple[1] = value;
+        fixConflict = true;
+      }
+    }
+  };
+  hashFunc(str, max) {
+    let hash = 0;
+    for (let i = 0;i < str.length; i++) {
+      let letter = str[i];
+      hash = (hash << 5) + letter.charCodeAt(0);
+      hash = (hash & hash) % max;
+    }
+    return hash;
+  };
 }
 module.exports = HashTable;
