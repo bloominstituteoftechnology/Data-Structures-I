55a627abf5f10087d437e05e80218c8158d792db
Moises Dobarganes <designerexpert@gmail.com>
2017-11-14 15:52:38 -0500

    Updating Hash Tables with Peer Edits

--- src/hash-table.js
+++ src/hash-table.js
@@ -11,56 +11,47 @@ class HashTable {
     this.storage = new LimitedArray(this.limit);
     // Do not modify anything inside of the constructor
   }
-
-  insert(key, value) {
-    // Adds the given key, value pair to the hash table
-    // Fetch the bucket associated with the given key using the getIndexBelowMax function
-    // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
-    // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
-    const dataIndex = getIndexBelowMax(key.toString(), this.limit);
-    const keyVal = value;
-    // this.storage.each((content, index) => {
-    //   if (content !== keyVal) {
-    //     //console.log(content);
-    //     const contentDataIndex = getIndexBelowMax(key.toString(), this.limit);
-    //     const colide = this.storage.get(contentDataIndex);
-    //     if (colide.length > 1)
-    //     //console.log(colide);
-    //     const concatColide = [keyVal, colide];
-    //     //console.log(concatColide);
-    //     this.storage.set(dataIndex, concatColide);
-    //     console.log(this.retrieve(key))
-    //   } else {
-    //     this.storage.set(dataIndex, keyVal);
-    //   }
-    // });
-    this.storage.set(dataIndex, keyVal);
+  // Adds the given key, value pair to the hash table
+  // Fetch the bucket associated with the given key using the getIndexBelowMax function
+  // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
+  // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
+  insert(rawKey, value) {
+    const key = String(rawKey);
+    const insertionKey = getIndexBelowMax(key, this.limit);
+    let bucket = this.storage.get(insertionKey);
+    if (!bucket) {
+      bucket = {};
+      bucket[key] = value;
+      this.storage.set(insertionKey, bucket);
+    } else {
+      bucket[key] = value;
+    }
   }
-
-  remove(key) {
-    // Removes the key, value pair from the hash table
-    // Fetch the bucket associated with the given key using the getIndexBelowMax function
-    // Remove the key, value pair from the bucket
-    const dataIndex = getIndexBelowMax(key.toString(), this.limit);
-    this.storage.set(dataIndex);
+  // Removes the key, value pair from the hash table
+  // Fetch the bucket associated with the given key using the getIndexBelowMax function
+  // Remove the key, value pair from the bucket
+  remove(rawKey) {
+    if (rawKey !== undefined) {
+      const key = String(rawKey);
+      const insertionKey = getIndexBelowMax(key, this.limit);
+      const bucket = this.storage.get(insertionKey);
+      if (typeof bucket === 'object' && bucket[key] !== 'undefined') {
+        bucket[key] = undefined;
+      }
+    }
   }
-
-  retrieve(key) {
-    const dataIndex = getIndexBelowMax(key.toString(), this.limit);
-    return this.storage.get(dataIndex);
-    // Fetches the value associated with the given key from the hash table
-    // Fetch the bucket associated with the given key using the getIndexBelowMax function
-    // Find the key, value pair inside the bucket and return the value
+  // Fetches the value associated with the given key from the hash table
+  // Fetch the bucket associated with the given key using the getIndexBelowMax function
+  // Find the key, value pair inside the bucket and return the value
+  retrieve(rawKey) {
+    const key = String(rawKey);
+    const insertionKey = getIndexBelowMax(key, this.limit);
+    const bucket = this.storage.get(insertionKey);
+    if (typeof bucket === 'object' && typeof bucket[key] !== 'undefined') {
+      return bucket[key];
+    }
+    return undefined;
   }
 }
-
-// const l = new HashTable();
-// const kb = (x, y) => { console.log(x, y); };
-// l.insert('name', 'Moises');
-// l.insert('name', 'Alex');
-// console.log(l.retrieve('name'));
-// console.log(l.storage.get(1));
-// console.log(l.storage.each(kb));
-
 
 module.exports = HashTable;
