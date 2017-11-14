69db5d769351e0158bd16866c7d48176106f203b
Jesse Hood <jessenoob@gmail.com>
2017-11-13 20:01:17 -0500

    Rewriting hash-table properly, tests passing except for numbers as keys and collisions

--- src/hash-table.js
+++ src/hash-table.js
@@ -9,6 +9,7 @@ class HashTable {
   constructor(limit = 8) {
     this.limit = limit;
     this.storage = new LimitedArray(this.limit);
+    this.length = 0;
     // Do not modify anything inside of the constructor
   }
   // Adds the given key, value pair to the hash table
@@ -17,24 +18,37 @@ class HashTable {
   // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
   insert(key, value) {
     const hash = getIndexBelowMax(key, this.limit);
-    if (typeof this.storage[hash] === 'undefined') this.storage[hash] = {};
-    this.storage[hash][key] = value;
+    this.storage.set(hash, value);
+    this.length++;
+    // this.adjustLimit();
+  }
+
+  // extra credit
+  adjustLimit() {
+    if (this.length >= this.limit * 0.75) {
+      this.limit = this.limit * 2;
+      /*
+      const newStorage = new LimitedArray(this.limit);
+      this.storage = newStorage;
+      */
+    }
   }
   // Removes the key, value pair from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Remove the key, value pair from the bucket
   remove(key) {
     const hash = getIndexBelowMax(key, this.limit);
-    if (typeof this.storage[hash] === 'undefined') return undefined;
-    delete this.storage[hash][key];
+    if (typeof this.storage.get(hash) === 'undefined') return undefined;
+    this.storage.set(hash, undefined);
+    this.length--;
   }
   // Fetches the value associated with the given key from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Find the key, value pair inside the bucket and return the value
   retrieve(key) {
     const hash = getIndexBelowMax(key, this.limit);
-    if (typeof this.storage[hash] === 'undefined') return undefined;
-    return this.storage[hash][key];
+    if (typeof this.storage.get(hash) === 'undefined') return undefined;
+    return this.storage.get(hash);
   }
 }
 module.exports = HashTable;
