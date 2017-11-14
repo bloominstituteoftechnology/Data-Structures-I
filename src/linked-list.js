82ed7d9f8bdf58ddb04413eaa18f4b2341943e1e
JacquelynnRohrich <33401573+JacquelynnRohrich@users.noreply.github.com>
2017-11-14 17:10:16 -0600

    finished

--- src/linked-list.js
+++ src/linked-list.js
@@ -9,17 +9,38 @@ class LinkedList {
   // If the list is empty, the new element is considered the tail as well as the head
   // If there is one element in the list before the new element is added, the new element becomes the tail of the list
   addToTail(value) {
-
+    const newTail = {
+      value,
+      next: null,
+    };
+    if (!this.head) {
+      this.head = newTail;
+      this.tail = newTail;
+    } else {
+      this.tail.next = newTail;
+      this.tail = newTail;
+    }
   }
   // Removes the current head node from the list, replacing it with the next element in the list
   // Returns the value of the removed node
   removeHead() {
-
+    if (!this.head) return null;
+    if (!this.head.next) this.tail = null;
+    const node = this.head.value;
+    this.head = this.head.next;
+    return node;
   }
   // Checks the linked list for the given value
   // Returns true if the the value is found in the list, false otherwise
   contains(value) {
-
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      if (currentNode.value === value) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
   }
 }
 
