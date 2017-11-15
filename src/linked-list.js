bf0fa44cb0b9d0e904d807c81c348ad970a8c082
David Peterman <dpeterman@na.hhcpr.hilton.com>
2017-11-15 06:28:45 -0500

    Dave: Linked list work

--- src/linked-list.js
+++ src/linked-list.js
@@ -9,17 +9,41 @@ class LinkedList {
   // If the list is empty, the new element is considered the tail as well as the head
   // If there is one element in the list before the new element is added, the new element becomes the tail of the list
   addToTail(value) {
-
+    const newNode = {
+      value,
+      next: null,
+    };
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.next = newNode;
+      this.tail = newNode;
+    }
   }
   // Removes the current head node from the list, replacing it with the next element in the list
   // Returns the value of the removed node
   removeHead() {
+    if (!this.head) return null;
+    this.length--;
+    if (!this.head.next) this.tail = null;
 
+    const returnValue = this.head.value;
+    this.head = this.head.next;
+    return returnValue;
   }
+
+  get size() {
+    return this.length;
+  }
+
   // Checks the linked list for the given value
   // Returns true if the the value is found in the list, false otherwise
   contains(value) {
-
+    const myList = new LinkedList();
+    myList.enqueue('Milo');
+    myList.enqueue('Plato');
+    myList.enqueue('Cujo');
   }
 }
 
