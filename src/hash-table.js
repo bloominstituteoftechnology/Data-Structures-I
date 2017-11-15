670ecc30c8a4e951b2d02e4a19d30242e97de233
William Antony <32254679+williampathadan@users.noreply.github.com>
2017-11-14 18:53:17 -0600

    Completed hash-table.js

--- src/hash-table.js
+++ src/hash-table.js
@@ -17,9 +17,13 @@ class HashTable {
   // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
   insert(key, value) {
     const hashIndex = getIndexBelowMax(key.toString(), this.limit);
-    if (typeof hashIndex === 'number') {
-      this.storage.storage[hashIndex] = value;
-    }
+
+    let bucket = this.storage.get(hashIndex) || [];
+    bucket = bucket.filter(pair => pair[0] !== key);
+    bucket.push([key, value]);
+
+    this.storage.set(hashIndex, bucket);
+
     if ((this.storage.length / this.limit) > 0.75) {
       this.limit *= 2;
       this.storage.limit *= 2;
@@ -30,8 +34,11 @@ class HashTable {
   // Remove the key, value pair from the bucket
   remove(key) {
     const hashIndex = getIndexBelowMax(key.toString(), this.limit);
-    if (this.storage.get(hashIndex)) {
-      delete this.storage.storage[hashIndex];
+    let bucket = this.storage.get(hashIndex);
+
+    if (bucket) {
+      bucket = bucket.filter(pair => pair[0] !== key);
+      this.storage.set(hashIndex, bucket);
     }
   }
   // Fetches the value associated with the given key from the hash table
@@ -39,24 +46,16 @@ class HashTable {
   // Find the key, value pair inside the bucket and return the value
   retrieve(key) {
     const hashIndex = getIndexBelowMax(key.toString(), this.limit);
-    const returnValue = this.storage.get(hashIndex);
-    return returnValue;
+    const bucket = this.storage.get(hashIndex);
+
+    let retrieved;
+    if (bucket) {
+      retrieved = bucket.filter(pair => pair[0] === key)[0];
+    }
+    return retrieved ? retrieved[1] : undefined;
   }
 }
 
 const myHash = new HashTable();
-
-myHash.insert('8', 'First Value');
-myHash.insert('HI!', 'Second Value');
-myHash.insert('H1!', 'Second Value');
-myHash.insert('H2!', 'Second Value');
-myHash.insert('H3!', 'Second Value');
-myHash.insert('H4!', 'Second Value');
-myHash.insert('H5!', 'Second Value');
-myHash.insert('H6!', 'Second Value');
-
-
-console.log(myHash.retrieve('8'));
-console.log(myHash.retrieve('HI!'));
 
 module.exports = HashTable;
