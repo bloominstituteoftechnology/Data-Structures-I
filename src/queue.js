7e75686d5bbc4e3f6fc776da082394ad07a1c2b3
Camila Daniels <camila.daniels926@gmail.com>
2017-12-08 20:34:17 -0600

    Camila Daniels camila.daniels926@gmail.com

--- src/queue.js
+++ src/queue.js
@@ -5,7 +5,35 @@
   4. Add a `dequeue` method that removes the item in the queue that was added earliest
 */
 class Queue {
+  constructor() {
+    this.storage = [];
+    this.size = 0;
+  }
 
+  size() {
+    return this.storage.length;
+  }
+
+  enqueue(item) {
+    this.size += 1;
+    return this.storage.unshift(item);
+  }
+
+  dequeue() {
+    return this.storage.pop();
+  }
 }
+
+const test = new Queue();
+
+// console.log(Object.getPrototypeOf(test).hasOwnProperty('enqueue'));
+// console.log(Object.getPrototypeOf(test).hasOwnProperty('dequeue'));
+// console.log(Object.getPrototypeOf(test).hasOwnProperty('size'));
+
+test.enqueue(true);
+test.enqueue('hi');
+test.enqueue(null);
+test.enqueue(77);
+console.log(test.dequeue());
 
 module.exports = Queue;
