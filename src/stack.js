58c430ef7037329429014a88830ab8f2f5d7db03
Camila Daniels <camila.daniels926@gmail.com>
2017-12-08 20:19:50 -0600

    Camila Daniels camila.daniels926@gmail.com

--- src/stack.js
+++ src/stack.js
@@ -6,6 +6,69 @@
 */
 class Stack {
 
+  constructor() {
+    this.storage = [];
+    this.size = 0;
+  }
+
+  size() {
+    return this.storage.length;
+  }
+
+  push(item) {
+    this.size += 1;
+    return this.storage.push(item);
+  }
+
+  pop() {
+    return this.storage.pop();
+  }
 }
 
+const test = new Stack();
+
+console.log(test.size);
+
+// console.log(Object.getPrototypeOf(test).hasOwnProperty('push'));
+// console.log(Object.getPrototypeOf(test).hasOwnProperty('pop'));
+// console.log(Object.getPrototypeOf(test).hasOwnProperty('size'));
+
+// test.push(null);
+// test.push(null);
+// test.push(null);
+// test.push(null);
+// test.push(null);
+// test.push(null);
+// test.push(null);
+// test.push(null);
+// test.push(null);
+// test.push(null);
+// console.log(test.size);
+
+// console.log(test.pop());
+
+// console.log(test.storage);
+
+// test.pop();
+// test.pop();
+// test.pop();
+// console.log(test.size);
+
+// test.push(1);
+// console.log(test.pop());
+
+// test.push(true);
+// test.push('hi');
+// test.push(null);
+// test.push(77);
+// console.log(test.pop());
+
+// test.push(true);
+// test.push('hi');
+// test.push(null);
+// test.push(77);
+// console.log(test.pop());
+// console.log(test.pop());
+// console.log(test.pop());
+// console.log(test.pop());
 module.exports = Stack;
