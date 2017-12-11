40a0b48c6c5ebaf534081939f924abf6c403f166
Samuel Cha <sunchipssam@gmail.com>
2017-12-11 12:06:23 -0800

    linked-list tests passed

--- src/linked-list.js
+++ src/linked-list.js
@@ -3,23 +3,47 @@ class LinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
-    // Do not modify anything inside of the constructor
   }
-  // Wraps the given value in a node object and adds the node to the tail of the list
-  // If the list is empty, the new element is considered the tail as well as the head
-  // If there is one element in the list before the new element is added, the new element becomes the tail of the list
+
   addToTail(value) {
-
+    const newNode = {
+      next: null,
+      value,
+    };
+    // check if head already exists (or if is empty)
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
   }
-  // Removes the current head node from the list, replacing it with the next element in the list
-  // Returns the value of the removed node
+
   removeHead() {
-
+    // no nodes
+    if (this.head === null) return;
+    // one node
+    if (this.head.next === null) {
+      const head = this.head;
+      this.head = null;
+      this.tail = null;
+      return head.value;
+    }
+    // multiple nodes
+    const head = this.head;
+    this.head = this.head.next;
+    return head.value;
   }
-  // Checks the linked list for the given value
-  // Returns true if the the value is found in the list, false otherwise
-  contains(value) {
 
+  contains(value) {
+    if (this.head === null) return false;
+    const searchLinkedList = (node) => {
+      if (node.value === value) return true;
+      if (node.next === null) return false;
+      return searchLinkedList(node.next);
+    };
+    return searchLinkedList(this.head);
   }
 }
 
