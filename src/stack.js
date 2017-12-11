a176551d485e12f67eff4ae7f34b597816520223
John C <johnjohnx4@gmail.com>
2017-12-11 16:10:23 -0500

    Committed wrong files originally, corrected with this commit

--- src/stack.js
+++ src/stack.js
@@ -1,11 +1,21 @@
-/*
-  1. Add a constructor with a storage structure; there are multiple options you could use for this
-  2. Add a size getter that returns the number of items the stack is storing
-  3. Add a `push` method that accepts an item as input and adds it to the storage structure
-  4. Add a `pop` method that removes the most recently-added item to the stack
-*/
 class Stack {
+  constructor() {
+    this.storage = [];
+    this.size = 0;
+  }
 
+  size() {
+    return this.size.length;
+  }
+
+  push(item) {
+    this.size++;
+    return this.storage.push(item);
+  }
+
+  pop() {
+    return this.storage.pop();
+  }
 }
 
 module.exports = Stack;
