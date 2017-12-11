e6d94071c72b1f66335973951fa7eb0c230d0799
John C <johnjohnx4@gmail.com>
2017-12-11 16:09:30 -0500

    Completed, all tests passing

--- src/queue.js
+++ src/queue.js
@@ -1,11 +1,23 @@
-/*
-  1. Add a constructor with a storage structure; there are multiple options you could use for this
-  2. Add a size getter that returns the number of items the queue is storing
-  3. Add an `enqueue` method that accepts an item as input and adds it to the storage structure
-  4. Add a `dequeue` method that removes the item in the queue that was added earliest
-*/
 class Queue {
+  constructor() {
+    this.storage = [];
+    this.size = 0;
+  }
 
+  size() {
+    return this.size.length;
+  }
+
+  enqueue(item) {
+    this.size++;
+    return this.storage.push(item);
+  }
+
+  dequeue() {
+    if (this.storage.length === 0) return;
+    this.size--;
+    return this.storage.shift();
+  }
 }
 
 module.exports = Queue;
