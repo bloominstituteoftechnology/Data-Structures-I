bdcacf0c3c75ce8c09e135be050fc6ea36b5c784
iradsanchez <ira.d.sanchez@gmail.com>
2017-12-11 16:39:11 -0700

    Ira Sanchez Dec 11 homework

--- src/stack.js
+++ src/stack.js
@@ -5,7 +5,25 @@
   4. Add a `pop` method that removes the most recently-added item to the stack
 */
 class Stack {
-
+  constructor() {
+    this.storage = [];
+  }
+  get size() {
+    // const storage = this.storage;
+    // return storage.length;  
+    return this.storage.length;  
+  }
+  push(item) {
+    return this.storage.length();
+  }
+  pop() {
+    // if it's empty, return null;
+    if (!this.storage) return null;
+    // store the value to be popped
+    const value = this.storage[this.storage.length - 1];
+    this.storage.pop();
+    return value;
+  }
 }
 
 module.exports = Stack;
