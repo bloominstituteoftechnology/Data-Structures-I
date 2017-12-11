8323054c9efc6fe8415231ffa49bcd79a8feae7d
Samuel Cha <sunchipssam@gmail.com>
2017-12-11 14:37:51 -0800

    stack and queue tests passed

--- src/queue.js
+++ src/queue.js
@@ -1,11 +1,21 @@
-/*
-  1. Add a constructor with a storage structure; there are multiple options you could use for this
-  2. Add a size getter that returns the number of items the queue is storing
-  3. Add an `enqueue` method that accepts an item as input and adds it to the storage structure
-  4. Add a `dequeue` method that removes the item in the queue that was added earliest
-*/
 class Queue {
+  constructor() {
+    this.queue = [];
+  }
+  get size() {
+    return this.queue.length;
+  }
 
+  enqueue(item) {
+    if (this.size === 0) this.queue[0] = item;
+    else {
+      this.queue.unshift(item);
+    }
+  }
+
+  dequeue() {
+    return this.queue.splice(this.size - 1, 1)[0];
+  }
 }
 
 module.exports = Queue;
--- src/stack.js
+++ src/stack.js
@@ -1,11 +1,20 @@
-/*
-  1. Add a constructor with a storage structure; there are multiple options you could use for this
-  2. Add a size getter that returns the number of items the stack is storing
-  3. Add a `push` method that accepts an item as input and adds it to the storage structure
-  4. Add a `pop` method that removes the most recently-added item to the stack
-*/
 class Stack {
+  constructor() {
+    this.stack = [];
+  }
+  get size() {
+    return this.stack.length;
+  }
 
+  push(item) {
+    this.stack[this.size] = item;
+  }
+
+  pop() {
+    const popItem = this.stack[this.size - 1];
+    this.stack = this.stack.splice(0, this.size - 1);
+    return popItem;
+  }
 }
 
 module.exports = Stack;
