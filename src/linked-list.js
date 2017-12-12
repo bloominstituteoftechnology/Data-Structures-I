55d601b3e52c580345f7ac04752bf14e9d7164b0
Samuel Cha <sunchipssam@gmail.com>
2017-12-12 11:36:26 -0800

    rearranged linked-list to be single linked at top and doubly linked at bottom of js file

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,26 +1,61 @@
 /* eslint-disable class-methods-use-this */
+
+// // single linked list implementation
+// class LinkedList {
+//   constructor() {
+//     this.head = null;
+//     this.tail = null;
+//   }
+
+//   addToTail(value) {
+//     const newNode = {
+//       next: null,
+//       value,
+//     };
+//     // check if head already exists (or if is empty)
+//     if (this.head === null) {
+//       this.head = newNode;
+//       this.tail = newNode;
+//       return;
+//     }
+//     this.tail.next = newNode;
+//     this.tail = newNode;
+//   }
+
+//   removeHead() {
+//     // no nodes
+//     if (this.head === null) return;
+//     // one node
+//     if (this.head.next === null) {
+//       const head = this.head;
+//       this.head = null;
+//       this.tail = null;
+//       return head.value;
+//     }
+//     // multiple nodes
+//     const head = this.head;
+//     this.head = this.head.next;
+//     return head.value;
+//   }
+
+//   contains(value) {
+//     if (this.head === null) return false;
+//     const searchLinkedList = (node) => {
+//       if (node.value === value) return true;
+//       if (node.next === null) return false;
+//       return searchLinkedList(node.next);
+//     };
+//     return searchLinkedList(this.head);
+//   }
+// }
+
+// doubly linked list implementation
 class LinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
   }
 
-  // addToTail(value) {
-  //   const newNode = {
-  //     next: null,
-  //     value,
-  //   };
-  //   // check if head already exists (or if is empty)
-  //   if (this.head === null) {
-  //     this.head = newNode;
-  //     this.tail = newNode;
-  //     return;
-  //   }
-  //   this.tail.next = newNode;
-  //   this.tail = newNode;
-  // }
-
-  // doubly linked list
   addToTail(value) {
     const newNode = {
       previous: null,
@@ -39,23 +74,6 @@ class LinkedList {
     this.tail.previous = oldTail;
   }
 
-  // removeHead() {
-  //   // no nodes
-  //   if (this.head === null) return;
-  //   // one node
-  //   if (this.head.next === null) {
-  //     const head = this.head;
-  //     this.head = null;
-  //     this.tail = null;
-  //     return head.value;
-  //   }
-  //   // multiple nodes
-  //   const head = this.head;
-  //   this.head = this.head.next;
-  //   return head.value;
-  // }
-
-  // doubly linked list
   removeHead() {
     // no nodes
     if (this.head === null) return;
@@ -73,17 +91,6 @@ class LinkedList {
     return head.value;
   }
 
-  // contains(value) {
-  //   if (this.head === null) return false;
-  //   const searchLinkedList = (node) => {
-  //     if (node.value === value) return true;
-  //     if (node.next === null) return false;
-  //     return searchLinkedList(node.next);
-  //   };
-  //   return searchLinkedList(this.head);
-  // }
-
-  // doubly linked list
   // start from tail
   // search head <--- tail
   contains(value) {
