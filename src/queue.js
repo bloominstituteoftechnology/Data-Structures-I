aa41a353eb87300779c10189d5f8a310983bf60b
Samuel Cha <sunchipssam@gmail.com>
2017-12-12 11:58:33 -0800

    added and passed doubly linked list for queue.js

--- src/queue.js
+++ src/queue.js
@@ -1,3 +1,7 @@
+const LinkedList = require('./linked-list');
+
+// Array implementation
+/*
 class Queue {
   constructor() {
     this.queue = [];
@@ -15,6 +19,37 @@ class Queue {
 
   dequeue() {
     return this.queue.splice(this.size - 1, 1)[0];
+  }
+}
+*/
+
+// doubly linked list implementation
+class Queue {
+  constructor() {
+    this.queue = new LinkedList();
+  }
+  get size() {
+    // count from head <-- tail
+    // if no nodes
+    if (this.queue.head === null) return 0;
+    // if one node
+    if (this.queue.head === this.queue.tail) return 1;
+    // if more than one node
+    const countVia = (node) => {
+      // recur base case when
+      // current node = head
+      if (node.previous === null) return 1;
+      return 1 + countVia(node.previous);
+    };
+    return countVia(this.queue.tail);
+  }
+
+  enqueue(item) {
+    this.queue.addToTail(item);
+  }
+
+  dequeue() {
+    return this.queue.removeHead();
   }
 }
 
