dbff64de0a8fd485e13a7d909c9cc9c80e5ba23a
Clara Andrew <claraandrew@Claras-MacBook-Pro.local>
2017-12-12 13:26:48 -0500

    changes made to src files

--- src/hash-table.js
+++ src/hash-table.js
@@ -16,19 +16,59 @@ class HashTable {
   // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
   // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
   insert(key, value) {
-
+    // has our key and get an index into our limited array
+    const index = getIndexBelowMax(key.toString(), this.limit);
+    // use the index to fetch whatever is at the slot in our limited array
+    const bucket = this.storage.get(index);
+    // inspect what we got
+    // if there is nothing in tthe slot, insert our key value pair inside a newly- instantiated bucket
+    if (bucket === undefined) {
+      this.storage.set(index, [[key, value]]);
+      return;
+    }
+    // otherwise, we'll need to add our new key value pair to the bucket
+      // make sure to maintain the uniqueness of key property
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        bucket[i][1] = value;
+        this.storage.set(index, bucket);
+        return;
+      }
+    }
+    bucket.push([key, value]);
+    this.storage.set(index, bucket);
   }
   // Removes the key, value pair from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Remove the key, value pair from the bucket
   remove(key) {
-
+    const index = getIndexBelowMax(key.toString(), this.limit);
+    const bucket = this.storage.get(index);
+    // if nothing in bucket return undefined
+    if (bucket === undefined) return undefined;
+    // if something is in bucket delete the [key, value]
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        bucket[i].splice(i, 1);
+        this.storage.set(index, bucket);
+        return;
+      }
+    }
   }
   // Fetches the value associated with the given key from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Find the key, value pair inside the bucket and return the value
   retrieve(key) {
-
+    const index = getIndexBelowMax(key.toString(), this.limit);
+    const bucket = this.storage.get(index);
+    // if nothing is in bucket return undefined
+    if (bucket === undefined) return undefined;
+    // get index -> iterate through array to find key, from key get value(only wants the value);
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        return bucket[i][1];
+      }
+    }
   }
 }
 
--- src/linked-list.js
+++ src/linked-list.js
@@ -1,4 +1,5 @@
 /* eslint-disable class-methods-use-this */
+
 class LinkedList {
   constructor() {
     this.head = null;
@@ -9,17 +10,55 @@ class LinkedList {
   // If the list is empty, the new element is considered the tail as well as the head
   // If there is one element in the list before the new element is added, the new element becomes the tail of the list
   addToTail(value) {
-
+    const newNode = {
+      next: null,
+      value,
+    };
+    // check if head already exists
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
   }
   // Removes the current head node from the list, replacing it with the next element in the list
   // Returns the value of the removed node
   removeHead() {
-
+    // there may be nothing in the list
+    if (this.head === null) return;
+    // there may be one node in our list
+    if (this.head.next === null) {
+      // create a new reference to our current head, so we can return the value
+      const head = this.head.value;
+      // set this.head to null
+      this.head = null;
+      // set this.tail to null
+      this.tail = null;
+      return head;
+    }
+    // there may be multiple nodes in our list
+    const head = this.head.value;
+    this.head = this.head.next;
+    return head;
   }
   // Checks the linked list for the given value
   // Returns true if the the value is found in the list, false otherwise
   contains(value) {
-
+    // if linked list is empty
+    if (this.head === null) return false;
+    // if list has elements
+    const searchLinkedList = (node) => {
+      // base case 1: if the current node we are looking at has the search value
+      if (node.value === value) return true;
+      // base case 2: if we've reached the end of our list and haven't found anything
+      if (node.next === null) return false;
+      // call our function recursively, passit it to the next node in our linked list
+      return searchLinkedList(node.next);
+    };
+    // call our recrusive function on our first linked list node
+    return searchLinkedList(this.head);
   }
 }
 
