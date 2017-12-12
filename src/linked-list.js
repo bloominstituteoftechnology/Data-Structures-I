207998b73e2fb95491ab36de7d55af09e519a521
Samuel Cha <sunchipssam@gmail.com>
2017-12-12 10:18:48 -0800

    linked-list with doubly linked list tests passed (using head <--- tail search method for contains(value)

--- src/linked-list.js
+++ src/linked-list.js
@@ -5,8 +5,25 @@ class LinkedList {
     this.tail = null;
   }
 
+  // addToTail(value) {
+  //   const newNode = {
+  //     next: null,
+  //     value,
+  //   };
+  //   // check if head already exists (or if is empty)
+  //   if (this.head === null) {
+  //     this.head = newNode;
+  //     this.tail = newNode;
+  //     return;
+  //   }
+  //   this.tail.next = newNode;
+  //   this.tail = newNode;
+  // }
+
+  // doubly linked list
   addToTail(value) {
     const newNode = {
+      previous: null,
       next: null,
       value,
     };
@@ -16,10 +33,29 @@ class LinkedList {
       this.tail = newNode;
       return;
     }
+    const oldTail = this.tail;
     this.tail.next = newNode;
     this.tail = newNode;
+    this.tail.previous = oldTail;
   }
 
+  // removeHead() {
+  //   // no nodes
+  //   if (this.head === null) return;
+  //   // one node
+  //   if (this.head.next === null) {
+  //     const head = this.head;
+  //     this.head = null;
+  //     this.tail = null;
+  //     return head.value;
+  //   }
+  //   // multiple nodes
+  //   const head = this.head;
+  //   this.head = this.head.next;
+  //   return head.value;
+  // }
+
+  // doubly linked list
   removeHead() {
     // no nodes
     if (this.head === null) return;
@@ -33,17 +69,32 @@ class LinkedList {
     // multiple nodes
     const head = this.head;
     this.head = this.head.next;
+    this.head.previous = null;
     return head.value;
   }
 
+  // contains(value) {
+  //   if (this.head === null) return false;
+  //   const searchLinkedList = (node) => {
+  //     if (node.value === value) return true;
+  //     if (node.next === null) return false;
+  //     return searchLinkedList(node.next);
+  //   };
+  //   return searchLinkedList(this.head);
+  // }
+
+  // doubly linked list
+  // start from tail
+  // search head <--- tail
   contains(value) {
     if (this.head === null) return false;
     const searchLinkedList = (node) => {
       if (node.value === value) return true;
-      if (node.next === null) return false;
-      return searchLinkedList(node.next);
+      // if we reach the head, return false
+      if (node.previous === null) return false;
+      return searchLinkedList(node.previous);
     };
-    return searchLinkedList(this.head);
+    return searchLinkedList(this.tail);
   }
 }
 
