d60ffa17b4455a51640ad2f35e43286af2e106ec
Samuel Cha <sunchipssam@gmail.com>
2017-12-11 19:07:11 -0800

    hash-table tests passed

--- src/hash-table.js
+++ src/hash-table.js
@@ -2,33 +2,29 @@
 /* eslint-disable class-methods-use-this */
 const { LimitedArray, getIndexBelowMax } = require('./hash-table-helpers');
 
-// LimitedArray, and getIndexBelowMax are two tools provided for you in the helper file.
-// There are other methods on the LimitedArray class in the './hash-table-helpers' file that you can use for your implementation.
-
 class HashTable {
   constructor(limit = 8) {
     this.limit = limit;
     this.storage = new LimitedArray(this.limit);
     // Do not modify anything inside of the constructor
   }
-  // Adds the given key, value pair to the hash table
-  // Fetch the bucket associated with the given key using the getIndexBelowMax function
-  // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
-  // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
+
   insert(key, value) {
-
+    const bucket = getIndexBelowMax(key, this.limit);
+    if (this.storage.get(bucket) === undefined) this.storage.set(bucket, {});
+    this.storage.get(bucket)[key] = value;
   }
-  // Removes the key, value pair from the hash table
-  // Fetch the bucket associated with the given key using the getIndexBelowMax function
-  // Remove the key, value pair from the bucket
+
   remove(key) {
-
+    const bucket = getIndexBelowMax(key, this.limit);
+    if (this.retrieve(key) === undefined) return undefined;
+    delete this.storage.get(bucket)[key];
   }
-  // Fetches the value associated with the given key from the hash table
-  // Fetch the bucket associated with the given key using the getIndexBelowMax function
-  // Find the key, value pair inside the bucket and return the value
-  retrieve(key) {
 
+  retrieve(key) {
+    const bucket = getIndexBelowMax(key, this.limit);
+    if (this.storage.get(bucket) === undefined) return undefined;
+    return this.storage.get(bucket)[key];
   }
 }
 
