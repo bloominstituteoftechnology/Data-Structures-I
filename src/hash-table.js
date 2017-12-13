dbb935fa780720dbde67b41c34c0bfb230f88e46
Synsyxx <synsyxx@github.com>
2017-12-13 10:31:05 -0600

    completed all assignments

--- src/hash-table.js
+++ src/hash-table.js
@@ -1,34 +1,90 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable class-methods-use-this */
-const { LimitedArray, getIndexBelowMax } = require('./hash-table-helpers');
-
-// LimitedArray, and getIndexBelowMax are two tools provided for you in the helper file.
-// There are other methods on the LimitedArray class in the './hash-table-helpers' file that you can use for your implementation.
+import { LimitedArray, getIndexBelowMax } from './hash-table-helpers';
 
 class HashTable {
-  constructor(limit = 8) {
-    this.limit = limit;
-    this.storage = new LimitedArray(this.limit);
-    // Do not modify anything inside of the constructor
-  }
-  // Adds the given key, value pair to the hash table
-  // Fetch the bucket associated with the given key using the getIndexBelowMax function
-  // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
-  // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
-  insert(key, value) {
+  constructor() {
+    this.limit = 8;
+    this.storage = new LimitedArray(this.limit).storage;
+    this.entries = 0;
+    this.changeHash = (newLimit, oldStorage) => {
+      const newStorage = new LimitedArray(newLimit).storage;
+      const bucketHashNumbers = Object.keys(oldStorage);
+      bucketHashNumbers.forEach((bucketHash) => {
+        const bucket = oldStorage[bucketHash];
+        if (Array.isArray(bucket)) {
+          bucket.forEach((keyvalue) => {
+            const key = keyvalue[0];
+            const value = keyvalue[1];
+            const i = getIndexBelowMax(key, newLimit);
+            if (!newStorage[i]) {
+              newStorage[i] = [];
+            }
 
-  }
-  // Removes the key, value pair from the hash table
-  // Fetch the bucket associated with the given key using the getIndexBelowMax function
-  // Remove the key, value pair from the bucket
-  remove(key) {
+            newStorage[i].push([key, value]);
+          });
+        }
+      });
 
+      return newStorage;
+    };
+    this.justHereToTest = () => {
+      return;
+    };
   }
-  // Fetches the value associated with the given key from the hash table
-  // Fetch the bucket associated with the given key using the getIndexBelowMax function
-  // Find the key, value pair inside the bucket and return the value
-  retrieve(key) {
 
+
+  insert(k, v) {
+    const i = getIndexBelowMax(k, this.limit);
+    if (!this.storage[i]) {
+      this.storage[i] = [];
+      this.storage[i].push([k, v]);
+      this.entries++;
+    } else if (this.storage[i].some(el => el[0] === k)) {
+      this.storage[i].forEach((el, z) => {
+        if (el[0] === k) {
+          el[1] = v;
+        }
+      });
+    } else {
+      this.storage[i].push([k, v]);
+      this.entries++;
+    }
+    if (this.entries > 0.75 * this.limit) {
+      this.limit *= 2;
+      this.storage = this.changeHash(this.limit, this.storage);
+    }
+  }
+
+  remove(k) {
+    if (getIndexBelowMax(k, this.limit)) {
+      const i = getIndexBelowMax(k, this.limit);
+      if (this.storage[i]) {
+        const bucket = this.storage[i];
+        for (let j = 0; j < bucket.length; j++) {
+          if (bucket[j][0] === k) {
+            const value = bucket[j][1];
+            this.storage[i] = this.storage[i].filter(el => el[0] !== k && el[1] !== value);
+            this.entries--;
+            if (this.entries < 0.25 * this.limit) {
+              this.limit /= 2;
+              this.storage = this.changeHash(this.limit, this.storage);
+            }
+            return value;
+          }
+        }
+      }
+    }
+    return this.remove;
+  }
+  retrieve(k) {
+    const i = getIndexBelowMax(k, this.limit);
+    if (this.storage[i]) {
+      const bucket = this.storage[i];
+      for (let j = 0; j < bucket.length; j++) {
+        if (bucket[j][0] === k) {
+          return bucket[j][1];
+        }
+      }
+    }
   }
 }
 
--- src/stack.js
+++ src/stack.js
@@ -5,7 +5,56 @@
   4. Add a `pop` method that removes the most recently-added item to the stack
 */
 class Stack {
+  constructor() {
+    this.items = {};
+    this.size = 0;
+    this.push = this.push;
+    this.pop = this.pop;
+  }
+  push(item) {
+    if (this.size === 0) {
+      this.items[this.size] = item;
+      this.size++;
+    } else {
+      const allItems = Object.keys(this.items).map(el => this.items[el]);
+      allItems.unshift(item);
+      this.size++;
 
+      for (let i = 0; i < allItems.length; i++) {
+        this.items[i] = allItems[i];
+      }
+    }
+  }
+
+  pop() {
+    if (this.size === 0) {
+      return 0;
+    }
+
+    let topObject;
+
+    const allItems = Object.keys(this.items).map((el, i) => {
+      if (i !== 0) {
+        return this.items[el];
+      }
+      return topObject = this.items[el];
+    });
+
+    allItems.shift();
+
+    for (let z = 0; z < allItems.length; z++) {
+      this.items[z] = allItems[z];
+    }
+
+    delete this.items[this.size - 1];
+    this.size--;
+
+    return topObject;
+  }
+
+  size() {
+    return this.size;
+  }
 }
 
 module.exports = Stack;
