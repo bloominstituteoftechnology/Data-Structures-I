229788322ed6b196b62c922c757cc4197a0e697c
Terrie Boeun Kim <TERR1E@Terries-MacBook-Pro.local>
2018-01-16 14:39:39 -0800

    Data-Structures-I assignment upload, partially finished

--- src/stack.js
+++ src/stack.js
@@ -5,7 +5,29 @@
   4. Add a `pop` method that removes the most recently-added item to the stack
 */
 class Stack {
+  constructor() {
+    this.count = 0;
+    this.storage = {};
+  }
 
+  push(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  pop() {
+    if (this.count === 0) {
+      return undefined;
+    }
+    this.count--;
+    const result = this.storage[this.count];
+    delete this.storage[this.count];
+    return result;
+  }
+
+  get size() {
+    return this.count;
+  }
 }
 
 module.exports = Stack;
