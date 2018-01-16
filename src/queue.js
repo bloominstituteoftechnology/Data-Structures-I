43d067930332b0cd29aee98816c7890374024096
Mephestys <mephestys@gmail.com>
2018-01-16 15:15:00 -0700

    Completed queue.js

--- src/queue.js
+++ src/queue.js
@@ -5,7 +5,29 @@
   4. Add a `dequeue` method that removes the item in the queue that was added earliest
 */
 class Queue {
+  constructor() {
+    this.size = 0;
+    this.lowestSize = 0;
+    this.storage = {};
+  }
 
+  size() {
+    return this.size - this.lowestSize;
+  }
+
+  enqueue(value) {
+    this.storage[this.size] = value;
+    ++this.size;
+  }
+
+  dequeue() {
+    if (this.size - this.lowestSize === 0) return undefined;
+
+    const result = this.storage[this.lowestSize];
+    delete this.storage[this.lowestSize];
+    ++this.lowestSize;
+    return result;
+  }
 }
 
 module.exports = Queue;
