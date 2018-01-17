fabc6a7afac998d2b7d91d64466e3f0df6bda8b3
Mephestys <mephestys@gmail.com>
2018-01-17 15:13:59 -0700

    hash-table.js nearly complete, does not yet handle collisions.

--- src/hash-table.js
+++ src/hash-table.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable class-methods-use-this */
+
 const { LimitedArray, getIndexBelowMax } = require('./hash-table-helpers');
 
 // LimitedArray, and getIndexBelowMax are two tools provided for you in the helper file.
@@ -11,24 +12,57 @@ class HashTable {
     this.storage = new LimitedArray(this.limit);
     // Do not modify anything inside of the constructor
   }
+
   // Adds the given key, value pair to the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
   // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
   insert(key, value) {
+    const index = getIndexBelowMax(key.toString(), this.limit);
+    const bucket = this.storage.get(index);
 
+    if (bucket === undefined) {
+      this.storage.set(index, [[key, value]]);
+      return;
+    }
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        bucket[i][1] = value;
+        this.storage.set(index, bucket);
+        return;
+      }
+    }
+
+    bucket.push([key, value]);
+    this.storage.set(index, bucket);
   }
+
   // Removes the key, value pair from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Remove the key, value pair from the bucket
   remove(key) {
+    const index = getIndexBelowMax(key.toString(), this.limit);
+    const bucket = this.storage.get(index);
 
+    if (bucket === undefined) return undefined;
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i] === key);
+      bucket[i].splice(bucket[i], 1);
+    }
   }
+
   // Fetches the value associated with the given key from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Find the key, value pair inside the bucket and return the value
   retrieve(key) {
+    const index = getIndexBelowMax(key.toString(), this.limit);
+    const result = this.storage.get(index);
 
+    if (result === undefined) return undefined;
+
+    return result[0][1];
   }
 }
 
