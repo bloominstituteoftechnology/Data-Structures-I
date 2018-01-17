391d67cbbb4236c08ee0afca059003a6ed73d51a
Dixie Korley <dixiekorley@gmail.com>
2018-01-17 13:39:32 -0500

    Completed homework.

--- src/hash-table.js
+++ src/hash-table.js
@@ -16,19 +16,53 @@ class HashTable {
   // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
   // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
   insert(key, value) {
-
+    const index = getIndexBelowMax(key, this.limit);
+    if (this.storage[index] === undefined) {
+      this.storage[index] = [];
+      this.storage[index].push([key, value]);
+    } else {
+      const bucket = this.storage[index];
+      let exists = false;
+      for (let i = 0; i < bucket.length; i++) {
+        if (bucket[i][0] === key) {
+          exists = true;
+          bucket[i][1] = value;
+          break;
+        }
+      }
+      if (!exists) this.storage[index].push([key, value]);
+    }
   }
   // Removes the key, value pair from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Remove the key, value pair from the bucket
   remove(key) {
-
+    const index = getIndexBelowMax(key, this.limit);
+    const bucket = this.storage[index];
+    if (bucket === [] || bucket === undefined) {
+      return;
+    }
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        bucket.splice(i, 1);
+        break;
+      }
+    }
   }
   // Fetches the value associated with the given key from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Find the key, value pair inside the bucket and return the value
   retrieve(key) {
-
+    const index = getIndexBelowMax(key, this.limit);
+    const bucket = this.storage[index];
+    if (bucket === [] || bucket === undefined) {
+      return;
+    }
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        return bucket[i][1];
+      }
+    }
   }
 }
 
