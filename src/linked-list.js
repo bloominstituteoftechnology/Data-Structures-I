d739ab896ec28282cb153369f64e492327329688
algorithmsrock <algorithmsrock@gmail.com>
2018-01-16 21:19:09 -0800

    Queue and Stack extra credit linkedlist and array complete

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,3 +1,4 @@
+/*eslint-disable */
 /* eslint-disable class-methods-use-this */
 class LinkedList {
   constructor() {
@@ -9,18 +10,65 @@ class LinkedList {
   // If the list is empty, the new element is considered the tail as well as the head
   // If there is one element in the list before the new element is added, the new element becomes the tail of the list
   addToTail(value) {
-
-  }
-  // Removes the current head node from the list, replacing it with the next element in the list
+   const newNode = {
+			next: null,
+      value: value,			
+  };
+	 if (this.head === null) {
+		 this.head = newNode;
+		 this.tail = newNode;
+     return;
+	}
+	 this.tail.next = newNode;
+	 this.tail = newNode;
+	}
+  addToHead(value) {
+		const newNode = {
+    next: this.head,
+			value: value,
+};
+ if (this.head === null) this.tail = newNode;
+ this.head = newNode;
+ }
+	// Removes the current head node from the list, replacing it with the next element in the list
   // Returns the value of the removed node
   removeHead() {
+   if (this.head === null) // need to check if there is a head node  
+		 return;
+   if (this.head.next === null) {
+		 const value = this.head.value;
+		 this.head = null;
+		 this.tail = null;
+		 return value;
+	}
+   const value = this.head.value;
+	 this.head = this.head.next;
+	 return value;
+	}
 
-  }
   // Checks the linked list for the given value
   // Returns true if the the value is found in the list, false otherwise
   contains(value) {
-
-  }
+     let node = this.head;
+		 while (node !== null) {
+ 			 if (node.value === value)
+ 				 return true;
+			 node = node.next;
+ 		}
+		 return false;
+	 }
 }
-
+//   contains(value) {
+	//	 if (this.head === null)
+		//	 return false;
+//		const searchLinkedList = (node) => {
+	//		if (node.value === value) 
+		//	return true;
+	//		if (node.next === null) 
+			//	return false;
+		//	return searchLinkedList(node.next);
+//		};
+	//	return searchLinkedList(this.head);
+//	}
+//}
 module.exports = LinkedList;
--- src/stack.js
+++ src/stack.js
@@ -1,11 +1,47 @@
+/*eslint-disable*/
 /*
   1. Add a constructor with a storage structure; there are multiple options you could use for this
   2. Add a size getter that returns the number of items the stack is storing
   3. Add a `push` method that accepts an item as input and adds it to the storage structure
   4. Add a `pop` method that removes the most recently-added item to the stack
 */
+const LinkedList = require('./linked-list.js');
 class Stack {
+  constructor() { 
+//	this.stacksize = 0;
+//  this.storage = [];		
+   this.storage = new LinkedList();
+  }
+//   get size () {
+	//	 return this.stacksize;
+//	 }
+ get size() {
+	 let node = this.storage.head;
+	 let stackcounter = 0;
+	 while (node !== null ){
+		 node = node.next;
+		 stackcounter++;
+	}
+	 return stackcounter;
+	}
 
+	 push(stackitem) {
+      this.storage.addToHead(stackitem);
+//		this.stacksize++;
+//	  this.storage[this.size] = stackitem;
+//		this.storage.push(stackitem);
 }
-
+//  delete this.storage[this.storage.stacksize] 
+  pop() {
+      return this.storage.removeHead();
+//	  if (this.stacksize > 0) {
+//		this.stacksize--;
+//	    const stackpop = this.storage[this.stacksize - 1];   
+//        this.storage.splice(this.stacksize - 1);              
+//        this.stacksize--; 
+//				return stackpop;
+	//		return this.storage.pop();
+//  }
+ }
+}
 module.exports = Stack;
