b8342245bccc42b32dfa568b69e5cb4033fd035b
Ronnie Miksch <fakepreferences@gmail.com>
2018-01-17 13:52:20 -0800

    attempting doubly linked lists

---
+++ src/doubly-linked-list.js
@@ -0,0 +1,41 @@
+class DoubleLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  addToTail(val) {
+    const newNode = {
+      next: null,
+      prev: null,
+      value: val,
+    };
+
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
+    }
+
+    this.tail.next = newNode;
+    newNode.prev = this.tail;
+    this.tail = newNode;
+  }
+
+  removeHead() {
+
+    if (this.head === null) return;
+
+    if (this.head.next === null) {
+      const val = this.head.value;
+      this.head = null;
+      this.tail = null;
+      return value;
+    }
+
+    const value = this.head.value;
+    this.head = this.head.next;   // i think this would work?
+
+    return value;
+  }
+}
