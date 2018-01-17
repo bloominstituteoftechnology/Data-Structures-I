a18fc72c186df184d3cf82259cc89b832f67ac74
Dani Tacheny <dani.tacheny@gmail.com>
2018-01-17 16:27:48 -0600

    passed tests

--- src/linked-list.js
+++ src/linked-list.js
@@ -8,18 +8,45 @@ class LinkedList {
   // Wraps the given value in a node object and adds the node to the tail of the list
   // If the list is empty, the new element is considered the tail as well as the head
   // If there is one element in the list before the new element is added, the new element becomes the tail of the list
-  addToTail(value) {
-
+  addToTail(val) {
+    const newNode = {
+      value: val,
+      next: null,
+    };
+    if (this.tail === null) {
+      this.tail = newNode;
+      this.head = newNode;
+      return;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
   }
   // Removes the current head node from the list, replacing it with the next element in the list
   // Returns the value of the removed node
   removeHead() {
-
+    if (this.head === null) {
+      return;
+    }
+    const value = this.head.value;
+    this.head = this.head.next;
+    return value;
   }
   // Checks the linked list for the given value
   // Returns true if the the value is found in the list, false otherwise
   contains(value) {
-
+    if (this.head === null) {
+      return false;
+    }
+    const find = (node) => {
+      if (node.value === value) {
+        return true;
+      }
+      if (node.next === null) {
+        return false;
+      }
+      return (find(node.next));
+    };
+    return find(this.head);
   }
 }
 
