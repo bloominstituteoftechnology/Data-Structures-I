90c9b3f9ebea725e4ea48ee7be79a82e66d4c86f
Peter Gray <peter@bpmmarketing.com>
2018-01-17 16:41:54 -0600

    hash tables

--- src/hash-table.js
+++ src/hash-table.js
@@ -16,19 +16,53 @@ class HashTable {
   // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
   // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
   insert(key, value) {
+    const index = getIndexBelowMax(key.toString(), this.limit);
+    const tempStore = this.storage.get(index);
 
+    if (tempStore === undefined) {
+      this.storage.set(index, [[key, value]]);
+      return;
+    }
+    for (let i = 0; i < tempStore.length; i++) {
+      if (tempStore[i][0] === key) {
+        tempStore[i][1] = value;
+        this.storage.set(index, tempStore);
+        return;
+      }
+    }
+    tempStore.push([key, value]);
+    this.storage.set(index, tempStore);
   }
   // Removes the key, value pair from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Remove the key, value pair from the bucket
   remove(key) {
-
+    const index = getIndexBelowMax(key.toString(), this.limit);
+    const tempStore = this.storage.get(index);
+    if (!tempStore) return;
+    if (tempStore.length === 1) {
+      this.storage.set(index, undefined);
+      return;
+    }
+    for (let i = 0; i < tempStore.length; i++) {
+      if (tempStore[i][0] === key) {
+        tempStore.splice(i, 1);
+        this.storage.set(index, tempStore);
+      }
+    }
   }
   // Fetches the value associated with the given key from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Find the key, value pair inside the bucket and return the value
   retrieve(key) {
-
+    const index = getIndexBelowMax(key.toString(), this.limit);
+    const tempStore = this.storage.get(index);
+    if (!tempStore) return;
+    for (let i = 0; i < tempStore.length; i++) {
+      if (tempStore[i][0] === key) {
+        return tempStore[i][1];
+      }
+    }
   }
 }
 
