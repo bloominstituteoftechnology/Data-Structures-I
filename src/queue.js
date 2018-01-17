50add54fdb231857567d82a217559c734447d18c
Peter Gray <peter@bpmmarketing.com>
2018-01-16 20:10:57 -0600

    queues

--- src/queue.js
+++ src/queue.js
@@ -5,7 +5,26 @@
   4. Add a `dequeue` method that removes the item in the queue that was added earliest
 */
 class Queue {
-
+  constructor() {
+    this.store = {};
+    this.start = -1;
+    this.end = -1;
+  }
+  get size() {
+    if (this.end === -1) return 0;
+    return this.end - this.start;
+  }
+  enqueue(value) {
+    this.store[++this.end] = value;
+  }
+  dequeue() {
+    if (this.size) {
+      const dequeued = this.store[++this.start];
+      delete this.store[this.start];
+      return dequeued;
+    }
+  }
 }
+
 
 module.exports = Queue;
