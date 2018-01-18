254a4b96f4cc645248791b304304ea35bb6bdc48
jonbrunt <jbrunt.mobile@gmail.com>
2018-01-18 08:32:06 -0600

    All tests passed

--- src/hash-table.js
+++ src/hash-table.js
@@ -16,19 +16,41 @@ class HashTable {
   // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
   // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
   insert(key, value) {
-
+    const index = getIndexBelowMax(key, this.limit);
+    if (!this.storage.get(index)) {
+      this.storage.set(index, [[key, value]]);
+    } else {
+      const bucket = this.storage.get(index);
+      const targetIndex = bucket.findIndex(val => val[0] === key);
+      if (targetIndex !== -1) {
+        bucket[targetIndex][1] = value;
+      } else {
+        const newArr = bucket.concat([[key, value]]);
+        this.storage.set(index, newArr);
+      }
+    }
   }
   // Removes the key, value pair from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Remove the key, value pair from the bucket
   remove(key) {
-
+    const bucket = this.storage.get(getIndexBelowMax(key, this.limit));
+    if (bucket) {
+      const targetIndex = bucket.findIndex(el => el[0] === key);
+      if (targetIndex !== -1) {
+        bucket.splice(targetIndex, 1);
+      }
+    }
   }
   // Fetches the value associated with the given key from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Find the key, value pair inside the bucket and return the value
   retrieve(key) {
-
+    const bucket = this.storage.get(getIndexBelowMax(key, this.limit));
+    if (bucket) {
+      const targetIndex = bucket.findIndex(el => el[0] === key);
+      if (targetIndex !== -1) return bucket[targetIndex][1];
+    }
   }
 }
 
