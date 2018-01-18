cfc71aeb6244d90b5c423d29088a91eef534636d
Igor Yermak <igor.yermak@gmail.com>
2018-01-17 20:43:58 -0500

    Data Structures 1
    Linked-lists, queue,stack and hash table(in progress)

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,4 +1,5 @@
 /* eslint-disable class-methods-use-this */
+/* eslint-disable */
 class LinkedList {
   constructor() {
     this.head = null;
@@ -9,18 +10,46 @@ class LinkedList {
   // If the list is empty, the new element is considered the tail as well as the head
   // If there is one element in the list before the new element is added, the new element becomes the tail of the list
   addToTail(value) {
-
+    const newNode = {
+      next: null,
+      value,
+    };
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
   }
   // Removes the current head node from the list, replacing it with the next element in the list
   // Returns the value of the removed node
   removeHead() {
-
+    if (this.head === null) {
+      return;
+    }
+    //check if head has a next
+    if (this.head.next === null) {
+      const value = this.head.value
+      this.head == null;
+      this.tail = null;
+      return value;
+    }
+    const value = this.head.value;
+    this.head = this.head.next;
+    return value;
   }
   // Checks the linked list for the given value
   // Returns true if the the value is found in the list, false otherwise
   contains(value) {
+    let node = this.head;
+    while (node !== null) {
+      if (node.value === value)
+        return true;
+    }
+    node = node.next;
 
+    return false;
   }
 }
-
 module.exports = LinkedList;
--- src/stack.js
+++ src/stack.js
@@ -4,7 +4,31 @@
   3. Add a `push` method that accepts an item as input and adds it to the storage structure
   4. Add a `pop` method that removes the most recently-added item to the stack
 */
+/* eslint-disable */
 class Stack {
+  constructor() {
+    this.store = [];
+    this.count = 0;
+  }
+
+  get size() {
+    return this.count;
+  }
+
+  push(input) {
+    let l = this.size;
+    this.store[l] = input;
+    this.count++;
+    return this.store[this.size] = input;
+  }
+  pop() {
+    const l = this.size;
+    const popped = this.storage[l - 1];
+    this.storage[l - 1] = null;
+    this.count--;
+    return popped;
+    console.log(popped)
+  }
 
 }
 
