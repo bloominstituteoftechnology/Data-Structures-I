608008d58eec9251c37f809c1c318279da2c82d9
Andrew Jarrett <ahrjarrett@gmail.com>
2018-01-22 16:55:59 -0700

    passing: linked list

--- src/linked-list.js
+++ src/linked-list.js
@@ -5,21 +5,43 @@ class LinkedList {
     this.tail = null;
     // Do not modify anything inside of the constructor
   }
+
   // Wraps the given value in a node object and adds the node to the tail of the list
   // If the list is empty, the new element is considered the tail as well as the head
   // If there is one element in the list before the new element is added, the new element becomes the tail of the list
   addToTail(value) {
-
+    const node = {
+      value,
+      next: null
+    };
+    if (this.head === null) {
+      this.head = node;
+    }
+    else {
+      this.tail.next = node;
+    }
+    this.tail = node;
   }
+
   // Removes the current head node from the list, replacing it with the next element in the list
   // Returns the value of the removed node
   removeHead() {
-
+    const removed = this.head;
+    this.head = this.head.next;
+    return removed.value;
   }
+
   // Checks the linked list for the given value
   // Returns true if the the value is found in the list, false otherwise
   contains(value) {
-
+    let testNode = this.head;
+    while (testNode.next) {
+      if (testNode.value === value) {
+	return true;
+      }
+      testNode = testNode.next;
+    }
+    return false;
   }
 }
 
