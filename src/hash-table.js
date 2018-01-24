ea4dd0d5131f06259999b7baff08078366c12e52
Andrew Jarrett <ahrjarrett@gmail.com>
2018-01-24 10:19:32 -0700

    hash table: all tests passing (besides extra credit)

--- src/hash-table.js
+++ src/hash-table.js
@@ -12,37 +12,61 @@ class HashTable {
     // Do not modify anything inside of the constructor
   }
 
+  find(key) {
+    const hash = getIndexBelowMax(key, this.limit);
+
+    // if no bucket, initialize a bucket:
+    const bucket = this.storage[hash] || [];
+
+    let match;
+    let matchIndex;
+    bucket.forEach((item, index) => {
+      if (item[0] === key) {
+	match = item;
+	matchIndex = index;
+      }
+    });
+    return {
+      match,
+      bucket,
+      matchIndex,
+      hash
+    }
+  }
+
   // Adds the given key, value pair to the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
   // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
   insert(key, value) {
-    const hashedIndex = getIndexBelowMax(key, this.limit);
-    if (!this.storage[hashedIndex]) {
-      this.storage[hashedIndex] = [[key, value]];
+    const { match, bucket, hash } = this.find(key);
+
+    if (!bucket.length) {
+      this.storage[hash] = [[key, value]];
     } else {
-      // CHECK IF KEY ALREADY EXISTS IN THE BUCKET!
-      this.storage[hashedIndex][this.storage[hashedIndex].length] = [key, value];
+      this.storage[hash].push([key, value]);
     }
-
-    console.log(hashedIndex);
-    console.log(this.storage);
-
-  }
-
-  // Removes the key, value pair from the hash table
-  // Fetch the bucket associated with the given key using the getIndexBelowMax function
-  // Remove the key, value pair from the bucket
-  remove(key) {
-
   }
 
   // Fetches the value associated with the given key from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Find the key, value pair inside the bucket and return the value
   retrieve(key) {
-
+    try {
+      return this.find(key).match[1];
+    } catch(e) {
+      return undefined;
+    }
   }
+
+  // REMOVES the key, value pair from the hash table
+  // Fetch the bucket associated with the given key using the getIndexBelowMax function
+  // Remove the key, value pair from the bucket
+  remove(key) {
+    const { match, bucket, matchIndex } = this.find(key);
+    bucket.splice(matchIndex, 1);
+  }
+
 }
 
 module.exports = HashTable;
