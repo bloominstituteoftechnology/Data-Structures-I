387d23d630ef76d1ea8491892ab5ec0665bd42de
Alexander <alexander.botello@g.austincc.edu>
2018-01-24 19:23:26 -0600

    Implement a queue with a linkedlist

--- src/queue.js
+++ src/queue.js
@@ -1,3 +1,4 @@
+const DoublyLinkedList = require('./doubly-linked-list');
 /*
   1. Add a constructor with a storage structure; there are multiple options you could use for this
   2. Add a size getter that returns the number of items the queue is storing
@@ -6,7 +7,8 @@
 */
 class Queue {
   constructor() {
-    this.items = [];
+    // this.items = [];
+    this.items = new DoublyLinkedList();
     this.size = 0;
   }
   size() {
@@ -14,14 +16,16 @@ class Queue {
   }
   enqueue(item) {
     this.size++;
-    this.items.push(item);
+    // this.items.push(item);
+    this.items.addToTail(item);
   }
   dequeue() {
     if (this.size === 0) {
       return 0;
     }
     --this.size;
-    return this.items.shift();
+    // return this.items.shift();
+    return this.items.removeFromHead().value;
   }
 }
 
