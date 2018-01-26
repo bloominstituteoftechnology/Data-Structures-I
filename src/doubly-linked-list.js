d6ed4280010ffa1a7fe1652339015b7332581c82
Alexander <alexander.botello@g.austincc.edu>
2018-01-25 18:42:20 -0600

    Implement a double linked list

--- src/doubly-linked-list.js
+++ src/doubly-linked-list.js
@@ -36,7 +36,7 @@ class DoublyLinkedList {
       this.tail = node;
     }
   }
-  // Removes the current head node from the list, replacing it with the next element in the list
+  // Removes the head head node from the list, replacing it with the next element in the list
   // Returns the value of the removed node
   removeFromHead() {
     if (!this.head) return null;
@@ -68,41 +68,42 @@ class DoublyLinkedList {
   // Checks the linked list for the given value
   // Returns true if the the value is found in the list, false otherwise
   delete(node) {
+    if (this.head === this.tail) {
+      this.head = null;
+      this.head = null;
+    }
+    if (node.prev === null) {
+      this.head = this.head.next;
+      this.head.prev = null;
+    }
+    if (node.next === null) {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+    }
     let current = this.head;
-    while (current) {
-      if (current.value === node.value) {
-        current.prev.next = current.next;
-        current.next.prev = current.prev;
-        break;
-      }
+    while (current.value !== node.value) {
       current = current.next;
     }
+    current.next.prev = current.prev;
+    current.prev.next = current.next;
   }
 
   moveToFront(node) {
-    let current = node;
-    while (current) {
-      if (current === this.head) {
-        const temp = this.head;
-        this.head = node;
-        this.head.next = temp;
-        break;
-      }
-      current = current.prev;
+    let head = node;
+    while (head.prev) {
+      head = head.prev;
     }
+    this.head = node;
+    this.head.next = head;
   }
 
   moveToBack(node) {
-    let current = node;
-    while (current) {
-      if (current === this.tail) {
-        const temp = this.tail;
-        this.tail = node;
-        this.tail.prev = temp;
-        break;
-      }
-      current = current.next;
+    let head = node;
+    while (head.next) {
+      head = head.next;
     }
+    this.tail = node;
+    this.tail.prev = head;
   }
 }
 
