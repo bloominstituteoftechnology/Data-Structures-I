c536d48658479b4de10b8746de995df18b56ab82
sbrooks84591 <Sbrooks84591@hotmail.com>
2018-02-02 19:32:43 -0600

    stack completed

--- src/stack.js
+++ src/stack.js
@@ -4,8 +4,27 @@
   3. Add a `push` method that accepts an item as input and adds it to the storage structure
   4. Add a `pop` method that removes the most recently-added item to the stack
 */
+// stacks are Last In, First Out LIFO
 class Stack {
-
+// mimic the stack behavior using an array
+  constructor() {
+    this.storage = [];
+    this.count = 0;
+  }
+  get size() {
+    return this.count;
+  }
+  push(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+  pop() {
+    if (this.count === 0) return null;
+    --this.count;
+    const item = this.storage[this.count];
+    this.storage[this.count] = null;
+    return item;
+  }
 }
 
 module.exports = Stack;
