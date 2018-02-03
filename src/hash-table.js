22777d1f8376af6f4e25531fd36cf24e5f141dad
sbrooks84591 <Sbrooks84591@hotmail.com>
2018-02-03 00:27:51 -0600

    corrected linked lists

--- src/hash-table.js
+++ src/hash-table.js
@@ -14,12 +14,35 @@ class HashTable {
   // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
   // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
   insert(key, value) {
-    
+   // use the key in this.limit to find the inex
+   const hashIndex = getIndexBelowMax(key.toString(), this.limit); // gives index where key:value pair is in the hashtable
+   let bucket;
+   if (Array.isArray(this.storage.get(hashIndex))) {
+     bucket = this.storage.get(hashIndex);
+   } else {
+     bucket = []; // creates new bucket if none exists
+   }
+   let newTuple = [key, value]; // to handle when keys are in the same index
+   let replaced = false;
+   bucket = bucket.map((tuple) => {
+     if(tuple[0] === key) {
+       replaced = true; // this if tuple is replaced
+       return newTuple // tuple is how key:value pairs are stored
+     }
+     return tuple; 
+   })
+   if (!replaced) {
+     bucket.push(tuple) // if not replaced, push new tuple into the bucket
+   }
+
+   // to put the bucket back into the hash table
+   this.storage.set(hashIndex, bucket);
   }
   // Removes the key, value pair from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
-  // Remove the key, value pair from the bucket
-  remove(key) {
+  // Remove the key, value pair from the bucket. put the bucket back
+  // after removing the key from the bucket
+  remove(key) { 
     
   }
   
--- src/linked-list.js
+++ src/linked-list.js
@@ -10,36 +10,50 @@ class LinkedList {
   // If there is one element in the list before the new element is added, the new element becomes the tail of the list
   addToTail(value) {  // creates new node
     const newNode = {
-      value,
+      value, // when key and value have same name, eslint allows just name
       next: null,
     };
 
-    if (this.tail === null) { // adds new node to head and tail
-      this.tail = newNode;
+    if (this.tail === null) { // checks if there is a tail
+      this.tail = newNode;  // adds new node to head and tail
       this.head = newNode;
       return;
     }
     const holder = this.tail; // adds tail to exising node
-    holder.next = newNode;
+    holder.next = newNode; // changes pointer from null to the new node
     this.tail = newNode;
   }
   // Removes the current head node from the list, replacing it with the next element in the list
   // Returns the value of the removed node
   removeHead() {
-    if (this.head === null) { // determine if there is a head
-      return null;
+    if (this.head === null) return null; // determine if there is a head
+    const oldHead = this.head;
+    this.head = this.head.next; // moves pointer from old head to next
+     // if the head is the last node, both head and tail must be removed
+    if (this.head === null) {
+      this.tail = null;
     }
-    const node = this.head; // if there is a head, moves the pointer to next head
+    return oldHead.value;
+  }
+    /* const node = this.head; // if there is a head, moves the pointer to next head
     this.head = node.next;
     if (this.head === null) {
       this.tail = null; // removes the tail by setting it to null
     }
-    return node.value;
-  }
+    return node.value; */
+  // }
   // Checks the linked list for the given value
   // Returns true if the the value is found in the list, false otherwise
-  contains(value) {
-
+  contains(value) { // checks the entire linked list to see if contains the // value use an iterative process to check each node on the list
+    let node = this.head;
+    while (node !== null) {
+      if (node.value === value) { return true; }
+      node = node.next; // if node does not equal requested value, resets to next node in list
+    }
+    // for (let key in node) { // linked list - do not use
+    // this.head {value: 1, next: {value: 2, next: null}} // linked list do not use
+    // }
+    return false;
   }
 }
 
