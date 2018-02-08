409127192c1983774dacad74510de352f7ca8d5a
ala ayaad <ala_ayaad@yahoo.com>
2018-02-07 23:39:14 -0800

    Ala Ayyad CS7
    the queue and stack & the linked list straight forward but the hash table still stuck with

--- src/queue.js
+++ src/queue.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 /*
   1. Add a constructor with a storage structure; there are multiple options you could use for this
   2. Add a size getter that returns the number of items the queue is storing
@@ -5,7 +6,59 @@
   4. Add a `dequeue` method that removes the item in the queue that was added earliest
 */
 class Queue {
+  constructor() {
+    this.length = 0;
 
+    this.head = null;
+    this.tail = null;
+  }
+  enqueue(value) {
+    this.length++;
+    const newItem = {
+      value,
+      next: null,
+    };
+
+    if (!this.head) {
+      this.head = newItem;
+      this.tail = newItem;
+    } else {
+      this.tail.next = newItem;
+      this.tail = newItem;
+    }
+  }
+  dequeue() {
+    if (this.length === 0) return 0;
+
+    this.length--;
+
+    if (!this.head) return null;
+    if (!this.head.next) this.tail = null;
+
+    const returnVal = this.head.value;
+    this.head = this.head.next;
+    return returnVal;
+  }
+  get size() {
+    return this.length;
+  }
 }
-
-module.exports = Queue;
+// class Queue {
+//   constructor() {
+//     this.queue = [];
+//     this.size = 0;
+//   }
+//   enqueue(item) {
+//     this.queue.push(item);
+//     this.size++;
+//   }
+//   dequeue(item) {
+//     if (this.queue.length === 0) return 'empty';
+//     this.size--;
+//     return this.queue.shift();
+//   }
+//   size() {
+//     if (this.size < 0) return 0;
+//     return this.size;
+//   }
+// }
--- src/stack.js
+++ src/stack.js
@@ -5,7 +5,40 @@
   4. Add a `pop` method that removes the most recently-added item to the stack
 */
 class Stack {
+  constructor() {
+    this.length = 0;
 
+    this.head = null;
+    this.tail = null;
+  }
+  push(value) {
+    this.length++;
+    const newItem = {
+      value,
+      next: null,
+    };
+
+    if (this.head) {
+      this.head = newItem;
+      this.tail = newItem;
+    }
+  }
+  pop() {
+    if (this.length === 0) return 0;
+
+    this.length--;
+
+    if (!this.head) return null;
+    if (!this.head.next) this.tail = null;
+
+    const returnVal = this.head.value;
+    this.head = this.head.next;
+    return returnVal;
+  }
+  get size() {
+    return this.length;
+  }
 }
+
 
 module.exports = Stack;
