bb5ec25a263ebefc96df23ee08519733684ee556
Lisa Cee <three_nineteen@yahoo.com>
2018-02-10 12:03:25 -0800

    doubly-linked-list add to head

--- src/doubly-linked-list.js
+++ src/doubly-linked-list.js
@@ -1,6 +1,56 @@
+/* eslint-disable */
+
 class DoublyLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
   }
+  // should have the methods "addToHead", "addToTail", "removeFromHead", "removeFromTail", "delete", "moveToFront", and "moveToBack"'
+  addToHead(value) {
+    const node = {
+      value,
+      next : this.head,
+      prev: null,
+    }
+    if (!this.tail) {
+      this.tail = node;
+      this.head = node;
+      return;
+    }
+    this.head.prev = node;
+    this.head = node;
+  }
+
+  addToTail(value) {
+    const node = {
+      value,
+      next: null,
+      prev: this.tail,
+    }
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+    let oldTail = this.tail;
+    this.tail = node;
+    this.oldTail.next = node;
+  }
+
+  removeFromHead() {
+
+  }
+  removeFromTail() {
+
+  }
+  delete() {
+
+  }
+  moveToFront() {
+
+  }
+  moveToBack() {
+
+  }
 }
+
+module.exports = DoublyLinkedList;
