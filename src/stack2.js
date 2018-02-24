6978a7e2a7c4a5be00280bd01b5bd9fb7d4c679c
Lisa Cee <three_nineteen@yahoo.com>
2018-02-24 11:38:34 -0800

    stack2

---
+++ src/stack2.js
@@ -0,0 +1,12 @@
+/*
+  1. Add a constructor with a storage structure; there are multiple options you could use for this
+  2. Add a size getter that returns the number of items the stack is storing
+  3. Add a `push` method that accepts an item as input and adds it to the storage structure
+  4. Add a `pop` method that removes the most recently-added item to the stack
+*/
+
+function stack2(a, b) {
+  return a + b;
+}
+
+module.exports = stack2;
---
+++ tests/stack2.test.js
@@ -0,0 +1,7 @@
+/* eslint-disable*/
+
+const stack2 = require('stack2');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(stack2(1,2)).toBe(3);
+})
