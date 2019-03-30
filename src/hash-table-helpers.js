6741902999e6fdcffe937f44a16c12eb6b6a4829
darthMinimus <irishseanvaldivia@gmail.com>
2019-03-29 22:52:18 -0500

    Okay, hash table with objects as buckets seems to work fine but failing the following linter tests: ✕ should properly remove items (7ms)     ✕ should not throw an error when attempting to retrieve a non-existent value (1ms)     ✕ should not throw an error when removing a non-existent key (1ms)

--- src/hash-table-helpers.js
+++ src/hash-table-helpers.js
@@ -53,3 +53,4 @@ module.exports = {
   LimitedArray,
   getIndexBelowMax,
 };
+
--- src/hash-table.js
+++ src/hash-table.js
@@ -16,20 +16,34 @@ class HashTable {
   // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
   // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
   insert(key, value) {
-
+    const keyHash = getIndexBelowMax(key, this.limit);
+    // i've been here before; the first time I made hash tables the buckets were arrays
+    // i then implemented the bucket as linked lists...but this time I'm thinking...why not objects?
+    if (this.storage[keyHash] === undefined) {
+      this.storage[keyHash] = {};
+      this.storage[keyHash][key] = value;
+    } else {
+      this.storage[keyHash][key] = value;
+    }
   }
   // Removes the key, value pair from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Remove the key, value pair from the bucket
   remove(key) {
-
+    const keyHash = getIndexBelowMax(key, this.limit);
+    if (this.storage[keyHash][key] !== undefined) {
+      delete this.storage[keyHash][key];
+    }
   }
   // Fetches the value associated with the given key from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Find the key, value pair inside the bucket and return the value
   retrieve(key) {
-
+    const keyHash = getIndexBelowMax(key, this.limit);
+    if (this.storage[keyHash][key] === undefined) return null;
+    return this.storage[keyHash][key];
   }
 }
 
 module.exports = HashTable;
+
